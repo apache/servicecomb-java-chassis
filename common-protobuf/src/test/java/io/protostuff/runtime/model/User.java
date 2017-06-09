efabc4c997932d6851ef1c63d97e29b00b98bbe5
wujimin <zzzwjm@gmail.com>
2017-06-09 16:34:27 +0800

    删除无效的javadoc，避免发版本失败

--- common-protobuf/src/test/java/io/protostuff/runtime/model/User.java
+++ common-protobuf/src/test/java/io/protostuff/runtime/model/User.java
@@ -15,14 +15,6 @@
  */
 package io.protostuff.runtime.model;
 
-/**
- * <一句话功能简述>
- * <功能详细描述>
- * @author
- * @version  [版本号, 2017年6月2日]
- * @see  [相关类/方法]
- * @since  [产品/模块版本]
- */
 public class User {
     private String name;
 
