bc75cf51b883c8e63f9c60b1fff714c4a763df1b
seanyinx <sean.yin@huawei.com>
2017-06-16 12:40:20 +0800

    JAV-30 re-wrote discovery test to make it more meaningful and readable

--- demo/demo-spring-boot-discovery/demo-spring-boot-discovery-client/src/test/java/io/servicecomb/demo/discovery/client/DiscoveryClientIT.java
+++ demo/demo-spring-boot-discovery/demo-spring-boot-discovery-client/src/test/java/io/servicecomb/demo/discovery/client/DiscoveryClientIT.java
@@ -1,25 +1,55 @@
 package io.servicecomb.demo.discovery.client;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import org.junit.Before;
+import io.servicecomb.springboot.starter.provider.EnableServiceComb;
+import java.net.URI;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-import io.servicecomb.demo.TestMgr;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = DiscoveryClientIT.DiscoveryTestApplication.class)
 public class DiscoveryClientIT {
 
-	@Before
-	public void setUp() {
-		TestMgr.errors().clear();
-	}
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@Autowired
+	private LoadBalancerClient client;
+
+  private final RestTemplate restTemplate = new RestTemplate();
 
 	@Test
-	public void clientGetsNoError() throws Exception {
-		DiscoveryClient.main(new String[0]);
-		System.out.println(TestMgr.errors().toString());
-		assertThat(TestMgr.errors().isEmpty(), is(true));
+	public void getsRemoteServiceFromDiscoveryClient() throws Exception {
+    URI remoteUri = discoveryClient.getInstances("discoveryServer").get(0).getUri();
+
+    assertThat(remoteUri).isNotNull();
+
+    String response = restTemplate.getForObject(
+        remoteUri.toString() + "/greeting/sayhello/{name}",
+        String.class,
+        "Mike");
+
+    assertThat(response).isEqualTo("hello Mike");
 	}
 
+  @SpringBootApplication
+  @EnableServiceComb
+  @EnableDiscoveryClient
+  static class DiscoveryTestApplication {
+
+    public static void main(String[] args) throws Exception {
+      SpringApplication.run(DiscoveryTestApplication.class, args);
+    }
+  }
 }
