77b69b59cb2a2503fba34cea11c9f2e096672551
wujimin <zzzwjm@gmail.com>
2017-06-27 13:04:53 +0800

    [WIP] extract service center tasks
    extract tasks from RegistryUtils/RegistryThread:
    1.register microservice
    2.register microservice instance
    3.heartbeat
    4.watch
    
    now, only extract, did not used them.

---
+++ service-registry/src/main/java/io/servicecomb/serviceregistry/task/MicroserviceWatchTask.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicecomb.serviceregistry.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import com.google.common.eventbus.EventBus;
+
+import io.servicecomb.serviceregistry.api.Const;
+import io.servicecomb.serviceregistry.api.registry.Microservice;
+import io.servicecomb.serviceregistry.api.response.MicroserviceInstanceChangedEvent;
+import io.servicecomb.serviceregistry.client.ServiceRegistryClient;
+import io.servicecomb.serviceregistry.config.ServiceRegistryConfig;
+import io.servicecomb.serviceregistry.task.event.ExceptionEvent;
+import io.servicecomb.serviceregistry.task.event.RecoveryEvent;
+
+public class MicroserviceWatchTask extends AbstractTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MicroserviceWatchTask.class);
+
+    private ServiceRegistryConfig serviceRegistryConfig;
+
+    public MicroserviceWatchTask(EventBus eventBus, ServiceRegistryConfig serviceRegistryConfig,
+            ServiceRegistryClient srClient, Microservice microservice) {
+        super(eventBus, srClient, microservice);
+        this.serviceRegistryConfig = serviceRegistryConfig;
+    }
+
+    public void run() {
+        if (!needToWatch()) {
+            return;
+        }
+
+        if (StringUtils.isEmpty(microservice.getServiceId())) {
+            // can not watch now.
+            return;
+        }
+
+        srClient.watch(microservice.getServiceId(),
+                (event) -> {
+                    if (event.failed()) {
+                        eventBus.post(new ExceptionEvent(event.cause()));
+                        return;
+                    }
+
+                    MicroserviceInstanceChangedEvent changedEvent = event.result();
+                    if (isProviderInstancesChanged(changedEvent) && !serviceRegistryConfig.isWatch()) {
+                        return;
+                    }
+                    if (!isProviderInstancesChanged(changedEvent)
+                            && !serviceRegistryConfig.isRegistryAutoDiscovery()) {
+                        return;
+                    }
+
+                    onMicroserviceInstanceChanged(changedEvent);
+                },
+                open -> {
+                    eventBus.post(new RecoveryEvent());
+                },
+                close -> {
+                });
+    }
+
+    private void onMicroserviceInstanceChanged(MicroserviceInstanceChangedEvent changedEvent) {
+        switch (changedEvent.getAction()) {
+            case CREATE:
+                LOGGER.info("microservice {}/{} REGISTERED an instance {}, {}.",
+                        changedEvent.getKey().getAppId(),
+                        changedEvent.getKey().getServiceName(),
+                        changedEvent.getInstance().getInstanceId(),
+                        changedEvent.getInstance().getEndpoints());
+                break;
+            case DELETE:
+                LOGGER.info("microservice {}/{} UNREGISTERED an instance {}, {}.",
+                        changedEvent.getKey().getAppId(),
+                        changedEvent.getKey().getServiceName(),
+                        changedEvent.getInstance().getInstanceId(),
+                        changedEvent.getInstance().getEndpoints());
+                break;
+            case UPDATE:
+                LOGGER.info("microservice {}/{} UPDATE an instance {} status or metadata, {}.",
+                        changedEvent.getKey().getAppId(),
+                        changedEvent.getKey().getServiceName(),
+                        changedEvent.getInstance().getInstanceId(),
+                        changedEvent.getInstance().getEndpoints());
+                break;
+            default:
+                break;
+        }
+
+        eventBus.post(changedEvent);
+    }
+
+    private boolean needToWatch() {
+        return serviceRegistryConfig.isWatch() || serviceRegistryConfig.isRegistryAutoDiscovery();
+    }
+
+    private boolean isProviderInstancesChanged(MicroserviceInstanceChangedEvent changedEvent) {
+        return !Const.REGISTRY_APP_ID.equals(changedEvent.getKey().getAppId())
+                && !Const.REGISTRY_SERVICE_NAME.equals(changedEvent.getKey().getServiceName());
+    }
+}
---
+++ service-registry/src/test/java/io/servicecomb/serviceregistry/task/TestAbstractRegisterTask.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicecomb.serviceregistry.task;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.eventbus.EventBus;
+
+import io.servicecomb.foundation.common.utils.ReflectUtils;
+import io.servicecomb.serviceregistry.api.registry.Microservice;
+import io.servicecomb.serviceregistry.api.registry.MicroserviceInstance;
+import io.servicecomb.serviceregistry.client.ServiceRegistryClient;
+import mockit.Mocked;
+
+public class TestAbstractRegisterTask {
+    private EventBus eventBus;
+
+    private Microservice microservice;
+
+    @Before
+    public void setup() {
+        eventBus = new EventBus();
+
+        microservice = new Microservice();
+        microservice.setAppId("app");
+        microservice.setServiceName("ms");
+
+        microservice.setIntance(new MicroserviceInstance());
+    }
+
+    @Test
+    public void testHeartbeatSuccess(@Mocked ServiceRegistryClient srClient) {
+        MicroserviceRegisterTask registerTask = new MicroserviceRegisterTask(eventBus, srClient, microservice);
+        ReflectUtils.setField(registerTask, "registered", true);
+
+        MicroserviceInstanceHeartbeatTask heartbeatTask =
+            new MicroserviceInstanceHeartbeatTask(eventBus, srClient, microservice);
+        ReflectUtils.setField(heartbeatTask, "heartbeatResult", HeartbeatResult.SUCCESS);
+
+        Assert.assertEquals(true, registerTask.isRegistered());
+        eventBus.post(heartbeatTask);;
+        Assert.assertEquals(true, registerTask.isRegistered());
+        Assert.assertEquals(eventBus, registerTask.getEventBus());
+    }
+
+    @Test
+    public void testHeartbeatFailed(@Mocked ServiceRegistryClient srClient) {
+        MicroserviceRegisterTask registerTask = new MicroserviceRegisterTask(eventBus, srClient, microservice);
+        ReflectUtils.setField(registerTask, "registered", true);
+
+        MicroserviceInstanceHeartbeatTask heartbeatTask =
+            new MicroserviceInstanceHeartbeatTask(eventBus, srClient, microservice);
+        ReflectUtils.setField(heartbeatTask, "heartbeatResult", HeartbeatResult.INSTANCE_NOT_REGISTERED);
+
+        Assert.assertEquals(true, registerTask.isRegistered());
+        eventBus.post(heartbeatTask);;
+        Assert.assertEquals(false, registerTask.isRegistered());
+    }
+
+    @Test
+    public void testHeartbeatOtherFailed(@Mocked ServiceRegistryClient srClient) {
+        MicroserviceRegisterTask registerTask = new MicroserviceRegisterTask(eventBus, srClient, microservice);
+        ReflectUtils.setField(registerTask, "registered", true);
+
+        Microservice otherMicroservice = new Microservice();
+        otherMicroservice.setAppId(microservice.getAppId());
+        otherMicroservice.setServiceName("ms1");
+
+        MicroserviceInstanceHeartbeatTask heartbeatTask =
+            new MicroserviceInstanceHeartbeatTask(eventBus, srClient, otherMicroservice);
+        ReflectUtils.setField(heartbeatTask, "heartbeatResult", HeartbeatResult.INSTANCE_NOT_REGISTERED);
+
+        Assert.assertEquals(true, registerTask.isRegistered());
+        eventBus.post(heartbeatTask);;
+        Assert.assertEquals(true, registerTask.isRegistered());
+    }
+}
