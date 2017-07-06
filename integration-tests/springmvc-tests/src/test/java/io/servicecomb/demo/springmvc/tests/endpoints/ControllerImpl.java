09bcc8f40af01ca82973737dc8489103b35b8a59
eric-lee-ltk <eric.lee.ltk@gmail.com>
2017-07-06 21:46:01 +0800

    JAV-88 added springmvc tests to verify springmvc endpoints only

---
+++ integration-tests/springmvc-tests/src/test/java/io/servicecomb/demo/springmvc/tests/endpoints/ControllerImpl.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.demo.springmvc.tests.endpoints;
+
+import io.servicecomb.demo.controller.Person;
+import io.servicecomb.provider.rest.common.RestSchema;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestSchema(schemaId = "controller")
+@RequestMapping(path = "/controller", produces = MediaType.APPLICATION_JSON)
+public class ControllerImpl {
+
+  @RequestMapping(path = "/add", method = RequestMethod.GET)
+  public int add(@RequestParam("a") int a, @RequestParam("b") int b) {
+    return a + b;
+  }
+
+  @RequestMapping(path = "/sayhello/{name}", method = RequestMethod.POST)
+  public String sayHello(@PathVariable("name") String name) {
+    return "hello " + name;
+  }
+
+  @RequestMapping(path = "/saysomething", method = RequestMethod.POST)
+  public String saySomething(String prefix, @RequestBody Person user) {
+    return prefix + " " + user.getName();
+  }
+
+  @RequestMapping(path = "/sayhi", method = RequestMethod.GET)
+  public String sayHi(HttpServletRequest request) {
+    String[] values = request.getParameterValues("name");
+    if (values != null && values.length > 0 && values[0].equals("throwexception")) {
+      throw new RuntimeException();
+    }
+    return "hi " + request.getParameter("name") + " " + Arrays.toString(values);
+  }
+
+  @RequestMapping(path = "/sayhei", method = RequestMethod.GET)
+  public String sayHei(@RequestHeader("name") String name) {
+    return "hei " + name;
+  }
+}
