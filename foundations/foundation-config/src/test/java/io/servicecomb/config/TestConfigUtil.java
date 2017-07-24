9e8f95902bc28296f66a2610ad52039edc5d5419
wujimin <zzzwjm@gmail.com>
2017-07-24 11:36:05 +0800

    JAV-196 configuration of config center url
    add test case for ConfigUtil lost scenes.

--- foundations/foundation-config/src/test/java/io/servicecomb/config/TestConfigUtil.java
+++ foundations/foundation-config/src/test/java/io/servicecomb/config/TestConfigUtil.java
@@ -19,19 +19,57 @@ package io.servicecomb.config;
 import java.util.List;
 
 import org.apache.commons.configuration.AbstractConfiguration;
+import org.apache.commons.configuration.Configuration;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.netflix.config.ConcurrentCompositeConfiguration;
+import com.netflix.config.DynamicWatchedConfiguration;
+import com.netflix.config.WatchedConfigurationSource;
+
 import io.servicecomb.config.archaius.sources.ConfigModel;
 import io.servicecomb.config.archaius.sources.MicroserviceConfigLoader;
+import io.servicecomb.foundation.common.utils.SPIServiceUtils;
+import mockit.Expectations;
+import mockit.Mocked;
 
 public class TestConfigUtil {
     @Test
-    public void testCreateDynamicConfig() {
+    public void testCreateConfigFromConfigCenterNoUrl(@Mocked Configuration localConfiguration) {
+        AbstractConfiguration configFromConfigCenter = ConfigUtil.createConfigFromConfigCenter(localConfiguration);
+        Assert.assertNull(configFromConfigCenter);
+    }
+
+    @Test
+    public void testCreateDynamicConfigNoConfigCenterSPI() {
         AbstractConfiguration dynamicConfig = ConfigUtil.createDynamicConfig();
         MicroserviceConfigLoader loader = ConfigUtil.getMicroserviceConfigLoader(dynamicConfig);
         List<ConfigModel> list = loader.getConfigModels();
         Assert.assertEquals(loader, ConfigUtil.getMicroserviceConfigLoader(dynamicConfig));
         Assert.assertEquals(1, list.size());
+        Assert.assertNotEquals(DynamicWatchedConfiguration.class,
+                ((ConcurrentCompositeConfiguration) dynamicConfig).getConfiguration(0).getClass());
+
+    }
+
+    @Test
+    public void testCreateDynamicConfigHasConfigCenter(
+            @Mocked WatchedConfigurationSource watchedConfigurationSource) {
+        new Expectations(SPIServiceUtils.class) {
+            {
+                SPIServiceUtils.getTargetService(WatchedConfigurationSource.class);
+                result = watchedConfigurationSource;
+            }
+        };
+
+        AbstractConfiguration dynamicConfig = ConfigUtil.createDynamicConfig();
+        Assert.assertEquals(DynamicWatchedConfiguration.class,
+                ((ConcurrentCompositeConfiguration) dynamicConfig).getConfiguration(0).getClass());
+    }
+
+    @Test
+    public void testGetPropertyInvalidConfig() {
+        Assert.assertNull(ConfigUtil.getProperty(null, "any"));
+        Assert.assertNull(ConfigUtil.getProperty(new Object(), "any"));
     }
 }
