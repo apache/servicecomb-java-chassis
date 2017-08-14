ebc3503e495cff65263b338a134cd995e68156e0
bao.liu <bao.liu@huawei.com>
2017-08-14 16:49:10 +0800

    [JAV-286]add protect feature when call remote calls in bean initialization

--- core/src/test/java/io/servicecomb/core/TestSystemBootListener.java
+++ core/src/test/java/io/servicecomb/core/TestSystemBootListener.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.servicecomb.core;
 
 import io.servicecomb.core.BootListener.BootEvent;
@@ -10,9 +25,6 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * Created by l00168639 on 2017/8/10.
- */
 public class TestSystemBootListener {
 
   @BeforeClass
