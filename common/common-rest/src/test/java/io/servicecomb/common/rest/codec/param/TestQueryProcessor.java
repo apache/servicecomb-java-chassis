4c4458584983b2ca0925679cb88c5fab8a4724bc
wujimin <wujimin@huawei.com>
2017-09-17 10:53:41 +0800

    [WIP] JAV-349 QueryProcessor parameter change to HttpServletRequest

---
+++ common/common-rest/src/test/java/io/servicecomb/common/rest/codec/param/TestQueryProcessor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.common.rest.codec.param;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import io.servicecomb.common.rest.codec.param.QueryProcessorCreator.QueryProcessor;
+import mockit.Expectations;
+import mockit.Mocked;
+
+public class TestQueryProcessor {
+  @Mocked
+  HttpServletRequest request;
+
+  private ParamValueProcessor createProcessor(String name, Class<?> type) {
+    return new QueryProcessor(name, TypeFactory.defaultInstance().constructType(type));
+  }
+
+  @Test
+  public void testGetValueNormal() throws Exception {
+    new Expectations() {
+      {
+        request.getParameter("name");
+        result = "value";
+      }
+    };
+
+    ParamValueProcessor processor = createProcessor("name", String.class);
+    Object value = processor.getValue(request);
+    Assert.assertEquals("value", value);
+  }
+
+  @Test
+  public void testGetValueContainerType() throws Exception {
+    new Expectations() {
+      {
+        request.getParameterValues("name");
+        result = new String[] {"value"};
+      }
+    };
+
+    ParamValueProcessor processor = createProcessor("name", String[].class);
+    String[] value = (String[]) processor.getValue(request);
+    Assert.assertThat(value, Matchers.arrayContaining("value"));
+  }
+
+  @Test
+  public void testGetProcessorType() {
+    ParamValueProcessor processor = createProcessor("name", String.class);
+    Assert.assertEquals("query", processor.getProcessorType());
+  }
+}
