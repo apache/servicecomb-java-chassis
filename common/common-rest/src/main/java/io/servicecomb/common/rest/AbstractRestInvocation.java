6d140de1b882367537da4e30537f44079addfb51
wujimin <wujimin@huawei.com>
2017-09-23 22:02:26 +0800

    JAV-355 create AbstractRestInvocation

---
+++ common/common-rest/src/main/java/io/servicecomb/common/rest/AbstractRestInvocation.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.common.rest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.Unpooled;
+import io.servicecomb.common.rest.codec.produce.ProduceProcessor;
+import io.servicecomb.common.rest.codec.produce.ProduceProcessorManager;
+import io.servicecomb.common.rest.definition.RestOperationMeta;
+import io.servicecomb.common.rest.filter.HttpServerFilter;
+import io.servicecomb.core.Const;
+import io.servicecomb.core.Invocation;
+import io.servicecomb.foundation.common.utils.JsonUtils;
+import io.servicecomb.foundation.vertx.http.HttpServletRequestEx;
+import io.servicecomb.foundation.vertx.http.HttpServletResponseEx;
+import io.servicecomb.foundation.vertx.stream.BufferOutputStream;
+import io.servicecomb.swagger.invocation.Response;
+import io.servicecomb.swagger.invocation.exception.InvocationException;
+
+public abstract class AbstractRestInvocation {
+  private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRestInvocation.class);
+
+  protected RestOperationMeta restOperationMeta;
+
+  protected Invocation invocation;
+
+  protected HttpServletRequestEx requestEx;
+
+  protected HttpServletResponseEx responseEx;
+
+  protected ProduceProcessor produceProcessor;
+
+  protected List<HttpServerFilter> httpServerFilters = Collections.emptyList();
+
+  public void setHttpServerFilters(List<HttpServerFilter> httpServerFilters) {
+    this.httpServerFilters = httpServerFilters;
+  }
+
+  protected boolean initProduceProcessor() {
+    produceProcessor = restOperationMeta.ensureFindProduceProcessor(requestEx);
+    if (produceProcessor == null) {
+      String msg = String.format("Accept %s is not supported", requestEx.getHeader(HttpHeaders.ACCEPT));
+      InvocationException exception = new InvocationException(Status.NOT_ACCEPTABLE, msg);
+      sendFailResponse(exception);
+      return false;
+    }
+
+    return true;
+  }
+
+  protected void setContext() throws Exception {
+    String strCseContext = requestEx.getHeader(Const.CSE_CONTEXT);
+    if (StringUtils.isEmpty(strCseContext)) {
+      return;
+    }
+
+    @SuppressWarnings("unchecked")
+    Map<String, String> cseContext =
+        JsonUtils.readValue(strCseContext.getBytes(StandardCharsets.UTF_8), Map.class);
+    invocation.setContext(cseContext);
+  }
+
+  protected void invoke() {
+    try {
+      this.initProduceProcessor();
+      this.setContext();
+      invocation.getHandlerContext().put(RestConst.REST_REQUEST, requestEx);
+
+      for (HttpServerFilter filter : httpServerFilters) {
+        Response response = filter.afterReceiveRequest(invocation, requestEx);
+        if (response != null) {
+          sendResponseQuietly(response);
+          return;
+        }
+      }
+
+      doInvoke();
+    } catch (Throwable e) {
+      sendFailResponse(e);
+    }
+  }
+
+  protected abstract void doInvoke() throws Throwable;
+
+  public void sendFailResponse(Throwable throwable) {
+    if (produceProcessor == null) {
+      produceProcessor = ProduceProcessorManager.DEFAULT_PROCESSOR;
+    }
+
+    Response response = Response.createProducerFail(throwable);
+    sendResponseQuietly(response);
+  }
+
+  protected void sendResponseQuietly(Response response) {
+    try {
+      sendResponse(response);
+    } catch (Throwable e) {
+      LOGGER.error("Failed to send rest response, operation:{}.",
+          invocation.getMicroserviceQualifiedName(),
+          e);
+    } finally {
+      requestEx.getAsyncContext().complete();
+    }
+  }
+
+  @SuppressWarnings("deprecation")
+  protected void sendResponse(Response response) throws Exception {
+    responseEx.setStatus(response.getStatusCode(), response.getReasonPhrase());
+    responseEx.setContentType(produceProcessor.getName());
+    if (response.getHeaders().getHeaderMap() != null) {
+      for (Entry<String, List<Object>> entry : response.getHeaders().getHeaderMap().entrySet()) {
+        for (Object value : entry.getValue()) {
+          responseEx.addHeader(entry.getKey(), String.valueOf(value));
+        }
+      }
+    }
+
+    Object body = response.getResult();
+    if (response.isFailed()) {
+      body = ((InvocationException) body).getErrorData();
+    }
+
+    try (BufferOutputStream output = new BufferOutputStream(Unpooled.compositeBuffer())) {
+      produceProcessor.encodeResponse(output, body);
+
+      responseEx.setBodyBuffer(output.getBuffer());
+      for (HttpServerFilter filter : httpServerFilters) {
+        filter.beforeSendResponse(invocation, responseEx);
+      }
+
+      responseEx.flushBuffer();
+    }
+  }
+}
---
+++ common/common-rest/src/test/java/io/servicecomb/common/rest/TestAbstractRestInvocation.java
@@ -0,0 +1,520 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.common.rest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.Response.Status;
+import javax.xml.ws.Holder;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.servicecomb.common.rest.codec.produce.ProduceProcessorManager;
+import io.servicecomb.common.rest.definition.RestOperationMeta;
+import io.servicecomb.common.rest.filter.HttpServerFilter;
+import io.servicecomb.core.Const;
+import io.servicecomb.core.Invocation;
+import io.servicecomb.core.definition.OperationMeta;
+import io.servicecomb.core.definition.SchemaMeta;
+import io.servicecomb.core.provider.consumer.ReferenceConfig;
+import io.servicecomb.foundation.common.utils.JsonUtils;
+import io.servicecomb.foundation.vertx.http.HttpServletRequestEx;
+import io.servicecomb.foundation.vertx.http.HttpServletResponseEx;
+import io.servicecomb.swagger.invocation.Response;
+import io.servicecomb.swagger.invocation.exception.InvocationException;
+import io.servicecomb.swagger.invocation.response.Headers;
+import io.vertx.core.buffer.Buffer;
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
+
+public class TestAbstractRestInvocation {
+  @Mocked
+  HttpServletRequestEx requestEx;
+
+  @Mocked
+  HttpServletResponseEx responseEx;
+
+  @Mocked
+  ReferenceConfig endpoint;
+
+  @Mocked
+  SchemaMeta schemaMeta;
+
+  @Mocked
+  OperationMeta operationMeta;
+
+  @Mocked
+  RestOperationMeta restOperation;
+
+  @Mocked
+  Object[] swaggerArguments;
+
+  Invocation invocation;
+
+  AbstractRestInvocation restInvocation = new AbstractRestInvocation() {
+    @Override
+    protected void doInvoke() throws Throwable {
+    }
+  };
+
+  @Before
+  public void setup() {
+    invocation = new Invocation(endpoint, operationMeta, swaggerArguments);
+
+    initRestInvocation();
+  }
+
+  private void initRestInvocation() {
+    restInvocation.produceProcessor = ProduceProcessorManager.JSON_PROCESSOR;
+    restInvocation.requestEx = requestEx;
+    restInvocation.responseEx = responseEx;
+    restInvocation.invocation = invocation;
+    restInvocation.restOperationMeta = restOperation;
+  }
+
+  @Test
+  public void setHttpServerFilters(@Mocked List<HttpServerFilter> httpServerFilters) {
+    restInvocation.setHttpServerFilters(httpServerFilters);
+
+    Assert.assertSame(httpServerFilters, restInvocation.httpServerFilters);
+  }
+
+  @Test
+  public void initProduceProcessorNull() {
+    new Expectations() {
+      {
+        restOperation.ensureFindProduceProcessor(requestEx);
+        result = null;
+      }
+    };
+
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+      }
+
+      @Override
+      public void sendFailResponse(Throwable throwable) {
+      }
+    };
+    initRestInvocation();
+
+    Assert.assertFalse(restInvocation.initProduceProcessor());
+  }
+
+  @Test
+  public void initProduceProcessorNormal() {
+    new Expectations() {
+      {
+        restOperation.ensureFindProduceProcessor(requestEx);
+        result = ProduceProcessorManager.JSON_PROCESSOR;
+      }
+    };
+
+    Assert.assertTrue(restInvocation.initProduceProcessor());
+  }
+
+  @Test
+  public void setContextNull() throws Exception {
+    new Expectations() {
+      {
+        requestEx.getHeader(Const.CSE_CONTEXT);
+        result = null;
+      }
+    };
+
+    Map<String, String> context = invocation.getContext();
+    restInvocation.setContext();
+    Assert.assertSame(context, invocation.getContext());
+  }
+
+  @Test
+  public void setContextEmpty() throws Exception {
+    new Expectations() {
+      {
+        requestEx.getHeader(Const.CSE_CONTEXT);
+        result = "";
+      }
+    };
+
+    Map<String, String> context = invocation.getContext();
+    restInvocation.setContext();
+    Assert.assertSame(context, invocation.getContext());
+  }
+
+  @Test
+  public void setContextNormal() throws Exception {
+    Map<String, String> context = new HashMap<>();
+    context.put("name", "value");
+    new Expectations() {
+      {
+        requestEx.getHeader(Const.CSE_CONTEXT);
+        result = JsonUtils.writeValueAsString(context);
+      }
+    };
+
+    restInvocation.setContext();
+    Assert.assertThat(invocation.getContext().size(), Matchers.is(1));
+    Assert.assertThat(invocation.getContext(), Matchers.hasEntry("name", "value"));
+  }
+
+  @Test
+  public void invokeFilterHaveResponse(@Mocked HttpServerFilter filter) {
+    Response response = Response.ok("");
+    new Expectations() {
+      {
+        filter.afterReceiveRequest(invocation, requestEx);
+        result = response;
+      }
+    };
+
+    Holder<Response> result = new Holder<>();
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+      }
+
+      @Override
+      protected void sendResponseQuietly(Response response) {
+        result.value = response;
+      }
+    };
+    initRestInvocation();
+    restInvocation.httpServerFilters = Arrays.asList(filter);
+
+    restInvocation.invoke();
+
+    Assert.assertSame(response, result.value);
+  }
+
+  @Test
+  public void invokeFilterNoResponse(@Mocked HttpServerFilter filter) {
+    new Expectations() {
+      {
+        filter.afterReceiveRequest(invocation, requestEx);
+        result = null;
+      }
+    };
+
+    Holder<Boolean> result = new Holder<>();
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+        result.value = true;
+      }
+    };
+    initRestInvocation();
+    restInvocation.httpServerFilters = Arrays.asList(filter);
+
+    restInvocation.invoke();
+
+    Assert.assertTrue(result.value);
+  }
+
+  @Test
+  public void invokeFilterException(@Mocked HttpServerFilter filter) {
+    Error error = new Error();
+    new Expectations() {
+      {
+        filter.afterReceiveRequest(invocation, requestEx);
+        result = error;
+      }
+    };
+
+    Holder<Throwable> result = new Holder<>();
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      public void sendFailResponse(Throwable throwable) {
+        result.value = throwable;
+      }
+
+      @Override
+      protected void doInvoke() throws Throwable {
+
+      }
+    };
+    initRestInvocation();
+    restInvocation.httpServerFilters = Arrays.asList(filter);
+
+    restInvocation.invoke();
+
+    Assert.assertSame(error, result.value);
+  }
+
+  @Test
+  public void invokeNormal(@Mocked HttpServerFilter filter) {
+    new Expectations() {
+      {
+        filter.afterReceiveRequest(invocation, requestEx);
+        result = null;
+      }
+    };
+
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+      }
+
+      @Override
+      public void sendFailResponse(Throwable throwable) {
+        throwable.printStackTrace();
+        Assert.fail("must not fail");
+      }
+    };
+    initRestInvocation();
+    restInvocation.httpServerFilters = Arrays.asList(filter);
+
+    restInvocation.invoke();
+  }
+
+  @Test
+  public void sendFailResponseNoProduceProcessor() {
+    restInvocation.produceProcessor = null;
+    restInvocation.sendFailResponse(new Error());
+
+    Assert.assertSame(ProduceProcessorManager.JSON_PROCESSOR, restInvocation.produceProcessor);
+  }
+
+  @Test
+  public void sendFailResponseHaveProduceProcessor() {
+    Holder<Response> result = new Holder<>();
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+      }
+
+      @Override
+      protected void sendResponseQuietly(Response response) {
+        result.value = response;
+      }
+    };
+    initRestInvocation();
+    restInvocation.produceProcessor = ProduceProcessorManager.PLAIN_PROCESSOR;
+
+    Throwable e = new InvocationException(Status.BAD_GATEWAY, "");
+    restInvocation.sendFailResponse(e);
+    Assert.assertSame(e, result.value.getResult());
+    Assert.assertSame(ProduceProcessorManager.PLAIN_PROCESSOR, restInvocation.produceProcessor);
+  }
+
+  @Test
+  public void sendResponseQuietlyNormal(@Mocked Response response) {
+    Holder<Response> result = new Holder<>();
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+      }
+
+      @Override
+      protected void sendResponse(Response response) throws Exception {
+        result.value = response;
+      }
+    };
+    initRestInvocation();
+
+    restInvocation.sendResponseQuietly(response);
+
+    Assert.assertSame(response, result.value);
+  }
+
+  @Test
+  public void sendResponseQuietlyException(@Mocked Response response) {
+    restInvocation = new AbstractRestInvocation() {
+      @Override
+      protected void doInvoke() throws Throwable {
+      }
+
+      @Override
+      protected void sendResponse(Response response) throws Exception {
+        throw new Error("");
+      }
+    };
+    initRestInvocation();
+
+    restInvocation.sendResponseQuietly(response);
+
+    // just log, check nothing
+  }
+
+  @Test
+  public void sendResponseStatusAndContentType(@Mocked Response response) throws Exception {
+    new Expectations() {
+      {
+        response.getStatusCode();
+        result = 123;
+        response.getReasonPhrase();
+        result = "reason";
+        response.getHeaders();
+        result = new Error("stop");
+      }
+    };
+
+    Map<String, Object> result = new HashMap<>();
+    responseEx = new MockUp<HttpServletResponseEx>() {
+      @Mock
+      void setStatus(int sc, String sm) {
+        result.put("statusCode", sc);
+        result.put("reasonPhrase", sm);
+      }
+
+      @Mock
+      void setContentType(String type) {
+        result.put("contentType", type);
+      }
+    }.getMockInstance();
+
+    Map<String, Object> expected = new HashMap<>();
+    expected.put("statusCode", 123);
+    expected.put("reasonPhrase", "reason");
+    expected.put("contentType", "application/json");
+
+    initRestInvocation();
+
+    try {
+      restInvocation.sendResponse(response);
+      Assert.fail("must throw exception");
+    } catch (Error e) {
+      Assert.assertEquals(expected, result);
+    }
+  }
+
+  @Test
+  public void testDoSendResponseHeaderNull(@Mocked Response response) throws Exception {
+    Headers headers = new Headers();
+
+    new Expectations() {
+      {
+        response.getResult();
+        result = new Error("stop");
+        response.getHeaders();
+        result = headers;
+      }
+    };
+
+    Headers resultHeaders = new Headers();
+    responseEx = new MockUp<HttpServletResponseEx>() {
+      @Mock
+      void addHeader(String name, String value) {
+        resultHeaders.addHeader(name, value);
+      }
+    }.getMockInstance();
+
+    initRestInvocation();
+
+    try {
+      restInvocation.sendResponse(response);
+      Assert.fail("must throw exception");
+    } catch (Error e) {
+      Assert.assertEquals(null, resultHeaders.getHeaderMap());
+    }
+  }
+
+  @Test
+  public void testDoSendResponseHeaderNormal(@Mocked Response response) throws Exception {
+    Headers headers = new Headers();
+    headers.addHeader("h1", "h1v1");
+    headers.addHeader("h1", "h1v2");
+    headers.addHeader("h2", "h2v");
+
+    new Expectations() {
+      {
+        response.getResult();
+        result = new Error("stop");
+        response.getHeaders();
+        result = headers;
+      }
+    };
+
+    Headers resultHeaders = new Headers();
+    responseEx = new MockUp<HttpServletResponseEx>() {
+      @Mock
+      void addHeader(String name, String value) {
+        resultHeaders.addHeader(name, value);
+      }
+    }.getMockInstance();
+    initRestInvocation();
+
+    try {
+      restInvocation.sendResponse(response);
+      Assert.fail("must throw exception");
+    } catch (Error e) {
+      Assert.assertEquals(headers.getHeaderMap(), resultHeaders.getHeaderMap());
+    }
+  }
+
+  @Test
+  public void testDoSendResponseResultOK(@Mocked Response response) throws Exception {
+    new Expectations() {
+      {
+        response.getResult();
+        result = "ok";
+      }
+    };
+
+    Buffer buffer = Buffer.buffer();
+    responseEx = new MockUp<HttpServletResponseEx>() {
+      @Mock
+      void setBodyBuffer(Buffer bodyBuffer) {
+        buffer.appendBuffer(bodyBuffer);
+      }
+    }.getMockInstance();
+    initRestInvocation();
+
+    restInvocation.sendResponse(response);
+    Assert.assertEquals("\"ok\"", buffer.toString());
+  }
+
+  @Test
+  public void testDoSendResponseResultOKFilter(@Mocked Response response)
+      throws Exception {
+    new Expectations() {
+      {
+        response.getResult();
+        result = "ok";
+      }
+    };
+
+    Buffer buffer = Buffer.buffer();
+    responseEx = new MockUp<HttpServletResponseEx>() {
+      @Mock
+      void setBodyBuffer(Buffer bodyBuffer) {
+        buffer.appendBuffer(bodyBuffer);
+      }
+    }.getMockInstance();
+
+    HttpServerFilter filter = new MockUp<HttpServerFilter>() {
+      @Mock
+      void beforeSendResponse(Invocation invocation, HttpServletResponseEx responseEx) {
+        buffer.appendString("-filter");
+      }
+    }.getMockInstance();
+
+    initRestInvocation();
+    restInvocation.setHttpServerFilters(Arrays.asList(filter));
+
+    restInvocation.sendResponse(response);
+    Assert.assertEquals("\"ok\"-filter", buffer.toString());
+  }
+}
