865daf3c222fcdbf1f9a0ddc3d17d0f21ded9dea
seanyinx <sean.yin@huawei.com>
2017-09-28 18:06:52 +0800

    JAV-389 included path and error in tracing

--- tracing/tracing-zipkin/src/main/java/io/servicecomb/tracing/zipkin/ZipkinTracingAdviser.java
+++ tracing/tracing-zipkin/src/main/java/io/servicecomb/tracing/zipkin/ZipkinTracingAdviser.java
@@ -6,29 +6,34 @@ import brave.Span;
 import brave.Tracer;
 import brave.Tracer.SpanInScope;
 
-public class ZipkinTracingAdviser {
+class ZipkinTracingAdviser {
+
+  private static final String CALL_PATH = "path";
 
   private final Tracer tracer;
 
-  public ZipkinTracingAdviser(Tracer tracer) {
+  ZipkinTracingAdviser(Tracer tracer) {
     this.tracer = tracer;
   }
 
-  public <T> T invoke(String spanName, Supplier<T> supplier) {
-    Span span = createSpan(spanName);
+  <T> T invoke(String spanName, String path, Supplier<T> supplier) {
+    Span span = createSpan(spanName, path);
     try (SpanInScope spanInScope = tracer.withSpanInScope(span)) {
       return supplier.get();
+    } catch (Throwable throwable) {
+      span.tag("error", throwable.getClass().getSimpleName() + ": " + throwable.getMessage());
+      throw throwable;
     } finally {
       span.finish();
     }
   }
 
-  private Span createSpan(String spanName) {
+  private Span createSpan(String spanName, String path) {
     Span currentSpan = tracer.currentSpan();
     if (currentSpan != null) {
-      return tracer.newChild(currentSpan.context()).name(spanName).start();
+      return tracer.newChild(currentSpan.context()).name(spanName).tag(CALL_PATH, path).start();
     }
 
-    return tracer.newTrace().name(spanName).start();
+    return tracer.newTrace().name(spanName).tag(CALL_PATH, path).start();
   }
 }
