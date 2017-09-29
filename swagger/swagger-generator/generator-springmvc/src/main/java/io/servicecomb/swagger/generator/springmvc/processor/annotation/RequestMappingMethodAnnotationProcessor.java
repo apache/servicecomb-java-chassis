2d9d43a84d04a7ef5343a17d88cf3625e87a8dde
eric-lee-ltk <lidagang1@huawei.com>
2017-09-29 19:47:56 +0800

    added simplified mapping annotations
    Signed-off-by: eric-lee-ltk <lidagang1@huawei.com>

--- swagger/swagger-generator/generator-springmvc/src/main/java/io/servicecomb/swagger/generator/springmvc/processor/annotation/RequestMappingMethodAnnotationProcessor.java
+++ swagger/swagger-generator/generator-springmvc/src/main/java/io/servicecomb/swagger/generator/springmvc/processor/annotation/RequestMappingMethodAnnotationProcessor.java
@@ -16,17 +16,14 @@
 
 package io.servicecomb.swagger.generator.springmvc.processor.annotation;
 
-import java.util.Arrays;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import io.servicecomb.swagger.generator.core.MethodAnnotationProcessor;
 import io.servicecomb.swagger.generator.core.OperationGenerator;
 import io.swagger.models.Operation;
 
-public class RequestMappingMethodAnnotationProcessor implements MethodAnnotationProcessor {
+public class RequestMappingMethodAnnotationProcessor extends AbstractHttpMethodMappingAnnotationProcessor {
 
   @Override
   public void process(Object annotation, OperationGenerator operationGenerator) {
@@ -47,22 +44,7 @@ public class RequestMappingMethodAnnotationProcessor implements MethodAnnotation
     }
   }
 
-  protected void processPath(String[] paths, OperationGenerator operationGenerator) {
-    if (null == paths || paths.length == 0) {
-      return;
-    }
-
-    // swagger仅支持配一个path，否则将会出现重复的operationId
-    if (paths.length > 1) {
-      throw new Error(String.format("not allowed multi path for %s:%s",
-          operationGenerator.getProviderMethod().getDeclaringClass().getName(),
-          operationGenerator.getProviderMethod().getName()));
-    }
-
-    operationGenerator.setPath(paths[0]);
-  }
-
-  protected void processMethod(RequestMethod[] requestMethods, OperationGenerator operationGenerator) {
+  private void processMethod(RequestMethod[] requestMethods, OperationGenerator operationGenerator) {
     if (null == requestMethods || requestMethods.length == 0) {
       return;
     }
@@ -74,22 +56,6 @@ public class RequestMappingMethodAnnotationProcessor implements MethodAnnotation
               operationGenerator.getProviderMethod().getName()));
     }
 
-    operationGenerator.setHttpMethod(requestMethods[0].name());
-  }
-
-  private void processConsumes(String[] consumes, Operation operation) {
-    if (null == consumes || consumes.length == 0) {
-      return;
-    }
-
-    operation.setConsumes(Arrays.asList(consumes));
-  }
-
-  protected void processProduces(String[] produces, Operation operation) {
-    if (null == produces || produces.length == 0) {
-      return;
-    }
-
-    operation.setProduces(Arrays.asList(produces));
+    super.processMethod(requestMethods[0], operationGenerator);
   }
 }
