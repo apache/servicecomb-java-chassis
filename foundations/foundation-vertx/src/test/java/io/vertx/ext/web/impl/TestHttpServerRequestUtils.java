43707ae7943037457f06b08bc844bbd404cfd393
wujimin <wujimin@huawei.com>
2017-10-11 17:27:20 +0800

    JAV-413 create edge base class

---
+++ foundations/foundation-vertx/src/test/java/io/vertx/ext/web/impl/TestHttpServerRequestUtils.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.vertx.ext.web.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.servicecomb.foundation.vertx.http.VertxServerRequestToHttpServletRequest;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.RoutingContext;
+import mockit.Expectations;
+import mockit.Mocked;
+
+public class TestHttpServerRequestUtils {
+  @Test
+  public void setPath(@Mocked HttpServerRequest request) {
+    HttpServerRequestWrapper wrapper = new HttpServerRequestWrapper(request);
+    HttpServerRequestUtils.setPath(wrapper, "abc");
+
+    Assert.assertEquals("abc", wrapper.path());
+  }
+
+  @Test
+  public void VertxServerRequestToHttpServletRequest(@Mocked RoutingContext context,
+      @Mocked HttpServerRequest request) {
+    HttpServerRequestWrapper wrapper = new HttpServerRequestWrapper(request);
+    new Expectations() {
+      {
+        context.request();
+        result = wrapper;
+      }
+    };
+
+    VertxServerRequestToHttpServletRequest reqEx = new VertxServerRequestToHttpServletRequest(context, "abc");
+    Assert.assertEquals("abc", reqEx.getRequestURI());
+  }
+}
---
+++ transports/transport-rest/transport-rest-vertx/src/main/java/io/servicecomb/transport/rest/vertx/edge/AbstractEdgeDispatcher.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.transport.rest.vertx.edge;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.servicecomb.transport.rest.vertx.AbstractVertxHttpDispatcher;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.RoutingContext;
+
+public abstract class AbstractEdgeDispatcher extends AbstractVertxHttpDispatcher {
+  private static final Logger LOGGER = LoggerFactory.getLogger(AbstractEdgeDispatcher.class);
+
+  protected void onFailure(RoutingContext context) {
+    LOGGER.error("edge server failed.", context.failure());
+    HttpServerResponse response = context.response();
+    response.setStatusCode(Status.BAD_GATEWAY.getStatusCode());
+    response.setStatusMessage(Status.BAD_GATEWAY.getReasonPhrase());
+    response.end();
+  }
+}
