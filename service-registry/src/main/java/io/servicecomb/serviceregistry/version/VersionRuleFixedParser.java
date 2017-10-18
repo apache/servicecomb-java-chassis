e27fa90cd6e89ba47d8bdf4095694355466058f9
wujimin <wujimin@huawei.com>
2017-10-18 15:59:59 +0800

    JAV-453 create fixed version rule

---
+++ service-registry/src/main/java/io/servicecomb/serviceregistry/version/VersionRuleFixedParser.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.serviceregistry.version;
+
+// fixed, this should be the last parser
+// will not check version format
+public class VersionRuleFixedParser implements VersionRuleParser {
+  class FixedVersionRule extends VersionRule {
+    private final Version version;
+
+    public FixedVersionRule(String versionRule, Version version) {
+      super(versionRule);
+
+      this.version = version;
+    }
+
+    public boolean isMatch(Version version, Version latestVersion) {
+      return this.version.equals(version);
+    }
+  }
+
+  @Override
+  public VersionRule parse(String strVersionRule) {
+    Version version = new Version(strVersionRule);
+    return new FixedVersionRule(version.getVersion(), version);
+  }
+}
