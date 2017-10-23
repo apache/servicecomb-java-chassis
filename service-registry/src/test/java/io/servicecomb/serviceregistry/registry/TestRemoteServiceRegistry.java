89866a0fcb36af619f60dbb03e0226e0137e3a97
wujimin <wujimin@huawei.com>
2017-10-23 10:01:52 +0800

    JAV-454 RemoteServiceRegistry support trigger pull by eventBus

--- service-registry/src/test/java/io/servicecomb/serviceregistry/registry/TestRemoteServiceRegistry.java
+++ service-registry/src/test/java/io/servicecomb/serviceregistry/registry/TestRemoteServiceRegistry.java
@@ -16,10 +16,15 @@
 package io.servicecomb.serviceregistry.registry;
 
 import java.util.ArrayList;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.google.common.eventbus.EventBus;
 
@@ -28,10 +33,16 @@ import io.servicecomb.foundation.common.net.IpPort;
 import io.servicecomb.serviceregistry.client.LocalServiceRegistryClientImpl;
 import io.servicecomb.serviceregistry.client.ServiceRegistryClient;
 import io.servicecomb.serviceregistry.config.ServiceRegistryConfig;
+import io.servicecomb.serviceregistry.consumer.MicroserviceVersions;
 import io.servicecomb.serviceregistry.definition.MicroserviceDefinition;
+import io.servicecomb.serviceregistry.task.event.PullMicroserviceVersionsInstancesEvent;
 import io.servicecomb.serviceregistry.task.event.ShutdownEvent;
+import mockit.Deencapsulation;
 import mockit.Expectations;
 import mockit.Injectable;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
 
 public class TestRemoteServiceRegistry {
   class TestingRemoteServiceRegistry extends RemoteServiceRegistry {
@@ -45,6 +56,9 @@ public class TestRemoteServiceRegistry {
       return new LocalServiceRegistryClientImpl();
     }
   }
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
   @Test
   public void testLifeCycle(@Injectable ServiceRegistryConfig config, @Injectable MicroserviceDefinition definition) {
@@ -87,5 +101,30 @@ public class TestRemoteServiceRegistry {
       }
     }, 0, TimeUnit.SECONDS);
     Assert.assertTrue(remote.getTaskPool().isShutdown());
+  }
+
+  @Test
+  public void onPullMicroserviceVersionsInstancesEvent(@Injectable ServiceRegistryConfig config,
+      @Injectable MicroserviceDefinition definition, @Mocked MicroserviceVersions microserviceVersions) {
+    PullMicroserviceVersionsInstancesEvent event = new PullMicroserviceVersionsInstancesEvent(microserviceVersions, 1);
+
+    ScheduledThreadPoolExecutor taskPool = new MockUp<ScheduledThreadPoolExecutor>() {
+      @Mock
+      ScheduledFuture<?> schedule(Runnable command,
+          long delay,
+          TimeUnit unit) {
+        Assert.assertEquals(1, delay);
+        throw new Error("ok");
+      }
+    }.getMockInstance();
+
+    expectedException.expect(Error.class);
+    expectedException.expectMessage(Matchers.is("ok"));
+
+    EventBus bus = new EventBus();
+    RemoteServiceRegistry remote = new TestingRemoteServiceRegistry(bus, config, definition);
+    bus.register(remote);
+    Deencapsulation.setField(remote, "taskPool", taskPool);
+    bus.post(event);
   }
 }
