0d2b2c2cae211b598d3821b434876ad82c7a6c48
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2017-10-23 11:03:59 +0800

    Return error to travis CI if maven commands failed

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -15,6 +15,12 @@ if [ "$1" == "install" ]; then
               	echo "Skipping the installation as it is tagged commit"
         else
                 mvn clean install -Ddocker.showLogs -Pdocker -Pjacoco -Pit -Pcoverage coveralls:report
+		if [ $? == 0 ]; then
+			echo "${green}Installation Success..${reset}"
+		else
+			echo "${red}Installation or Test Cases failed, please check the above logs for more details.${reset}"
+			exit 1
+		fi
         fi
         echo "Installation Completed"
 else
@@ -24,9 +30,21 @@ else
 		tar xvf gpg-sec.tar
 		echo "Deploying Staging Release"
 		mvn deploy -DskipTests -Prelease -Pdistribution -Ppassphrase --settings .travis.settings.xml
+		if [ $? == 0 ]; then
+			echo "${green}Staging Deployment is Success, please log on to Nexus Repo to see the staging release..${reset}"
+		else
+			echo "${red}Staging Release deployment failed.${reset}"
+			exit 1
+		fi
         else
 		echo "Deploy a Non-Signed Staging Release"
 		mvn deploy -DskipTests --settings .travis.settings.xml
+		if [ $? == 0 ]; then
+			echo "${green}Snapshot Deployment is Success, please log on to Nexus Repo to see the snapshot release..${reset}"
+		else
+			echo "${red}Snapshot deployment failed.${reset}"
+			exit 1
+		fi
                 
         fi
 	echo "Deployment Completed"
