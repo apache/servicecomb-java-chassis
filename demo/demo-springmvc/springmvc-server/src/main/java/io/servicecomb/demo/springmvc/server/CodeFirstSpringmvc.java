c1e99e94b3123939cd2f1cf71817f8e3a81e27f2
jeho0815 <jeho0815@email.com>
2017-10-26 17:36:48 +0800

    JAV-47 resolve if Invocation has an exception, then has no respose to client

--- demo/demo-springmvc/springmvc-server/src/main/java/io/servicecomb/demo/springmvc/server/CodeFirstSpringmvc.java
+++ demo/demo-springmvc/springmvc-server/src/main/java/io/servicecomb/demo/springmvc/server/CodeFirstSpringmvc.java
@@ -232,6 +232,18 @@ public class CodeFirstSpringmvc {
     }
     return name;
   }
+  
+  enum NameType {
+    abc,
+    def
+  }
+
+  @RequestMapping(path = "/testenum/{name}", method = RequestMethod.GET)
+  @ApiResponses(value = {@ApiResponse(code = 200, response = String.class, message = "200 normal"),
+      @ApiResponse(code = 490, response = String.class, message = "490 exception")})
+  public String testEnum(@RequestParam(name = "username") String username, @PathVariable(value = "name") NameType nameType) {
+    return nameType.toString();
+  }
 
   @RequestMapping(method = RequestMethod.POST, value = "/ignore")
   @ResponseBody
