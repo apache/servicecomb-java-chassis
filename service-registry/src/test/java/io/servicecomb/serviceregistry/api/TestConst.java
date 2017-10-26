7090e2e59d4505f8d12d394c21a0ad8c5f420ecd
bao.liu <bao.liu@huawei.com>
2017-10-26 15:02:46 +0800

    [JAV-461]service center API change. fix env parameters in CI make test fail.

--- service-registry/src/test/java/io/servicecomb/serviceregistry/api/TestConst.java
+++ service-registry/src/test/java/io/servicecomb/serviceregistry/api/TestConst.java
@@ -38,6 +38,7 @@ public class TestConst {
 
   @Test
   public void testV4URL() {
+
     // this test case depends on class loading and java property may initialized after class loading, so we make to run in all cases.
     if (Const.REGISTRY_API.CURRENT_VERSION.equals(Const.REGISTRY_API.VERSION_V3)) {
       Assert.assertEquals("/registry/v3/microservices", Const.REGISTRY_API.MICROSERVICE_OPERATION_ALL);
@@ -57,22 +58,23 @@ public class TestConst {
       Assert.assertEquals("/registry/v3/microservices/%s/instances/%s/properties",
           Const.REGISTRY_API.MICROSERVICE_INSTANCE_PROPERTIES);
     } else {
-      Assert.assertEquals("/v4/testtenane/registry/microservices", Const.REGISTRY_API.MICROSERVICE_OPERATION_ALL);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s", Const.REGISTRY_API.MICROSERVICE_OPERATION_ONE);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/instances",
+      String prefix = "/v4/" + Const.REGISTRY_API.TENANT_NAME + "/registry/";
+      Assert.assertEquals(prefix + "microservices", Const.REGISTRY_API.MICROSERVICE_OPERATION_ALL);
+      Assert.assertEquals(prefix + "microservices/%s", Const.REGISTRY_API.MICROSERVICE_OPERATION_ONE);
+      Assert.assertEquals(prefix + "microservices/%s/instances",
           Const.REGISTRY_API.MICROSERVICE_INSTANCE_OPERATION_ALL);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/instances/%s",
+      Assert.assertEquals(prefix + "microservices/%s/instances/%s",
           Const.REGISTRY_API.MICROSERVICE_INSTANCE_OPERATION_ONE);
-      Assert.assertEquals("/v4/testtenane/registry/existence", Const.REGISTRY_API.MICROSERVICE_EXISTENCE);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/schemas/%s",
+      Assert.assertEquals(prefix + "existence", Const.REGISTRY_API.MICROSERVICE_EXISTENCE);
+      Assert.assertEquals(prefix + "microservices/%s/schemas/%s",
           Const.REGISTRY_API.MICROSERVICE_SCHEMA);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/instances/%s/heartbeat",
+      Assert.assertEquals(prefix + "microservices/%s/instances/%s/heartbeat",
           Const.REGISTRY_API.MICROSERVICE_HEARTBEAT);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/watcher", Const.REGISTRY_API.MICROSERVICE_WATCH);
-      Assert.assertEquals("/v4/testtenane/registry/instances", Const.REGISTRY_API.MICROSERVICE_INSTANCES);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/properties",
+      Assert.assertEquals(prefix + "microservices/%s/watcher", Const.REGISTRY_API.MICROSERVICE_WATCH);
+      Assert.assertEquals(prefix + "instances", Const.REGISTRY_API.MICROSERVICE_INSTANCES);
+      Assert.assertEquals(prefix + "microservices/%s/properties",
           Const.REGISTRY_API.MICROSERVICE_PROPERTIES);
-      Assert.assertEquals("/v4/testtenane/registry/microservices/%s/instances/%s/properties",
+      Assert.assertEquals(prefix + "microservices/%s/instances/%s/properties",
           Const.REGISTRY_API.MICROSERVICE_INSTANCE_PROPERTIES);
     }
   }
