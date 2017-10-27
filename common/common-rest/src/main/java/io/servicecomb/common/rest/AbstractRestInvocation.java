d9f584a95f93b20f9e419b30f272e900d7341553
wujimin <wujimin@huawei.com>
2017-10-27 09:00:07 +0800

    log info optimized

--- common/common-rest/src/main/java/io/servicecomb/common/rest/AbstractRestInvocation.java
+++ common/common-rest/src/main/java/io/servicecomb/common/rest/AbstractRestInvocation.java
@@ -88,7 +88,7 @@ public abstract class AbstractRestInvocation {
 
       doInvoke();
     } catch (Throwable e) {
-      LOGGER.error("unknown edge exception.", e);
+      LOGGER.error("unknown rest exception.", e);
       sendFailResponse(e);
     }
   }
