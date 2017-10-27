44a5a0118525dd720182a58eef7c73e7b50393c7
bao.liu <bao.liu@huawei.com>
2017-10-27 18:15:24 +0800

    [JAV-461]service center API change. URI using domain and header using Project

--- service-registry/src/test/java/io/servicecomb/serviceregistry/api/TestConst.java
+++ service-registry/src/test/java/io/servicecomb/serviceregistry/api/TestConst.java
@@ -26,14 +26,14 @@ public class TestConst {
   @Before
   public void setUp() {
     System.setProperty("cse.service.registry.api.version", "v4");
-    System.setProperty("cse.config.client.tenantName", "testtenane");
+    System.setProperty("cse.config.client.domainName", "testtenane");
     ConfigUtil.createLocalConfig();
   }
 
   @After
   public void tearDown() {
     System.getProperties().remove("cse.service.registry.api.version");
-    System.getProperties().remove("cse.config.client.tenantName");
+    System.getProperties().remove("cse.config.client.domainName");
   }
 
   @Test
