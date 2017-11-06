213e7b0eb7735224e8721b38fff910f763b23b29
jeho0815 <jeho0815@email.com>
2017-11-06 20:20:15 +0800

    JAV-489 resolve Edge service return Cotent-Length error, if return the content-lengh from server, can't change the body, so remove this header

--- common/common-rest/src/main/java/io/servicecomb/common/rest/AbstractRestInvocation.java
+++ common/common-rest/src/main/java/io/servicecomb/common/rest/AbstractRestInvocation.java
@@ -141,7 +141,9 @@ public abstract class AbstractRestInvocation {
     if (response.getHeaders().getHeaderMap() != null) {
       for (Entry<String, List<Object>> entry : response.getHeaders().getHeaderMap().entrySet()) {
         for (Object value : entry.getValue()) {
-          responseEx.addHeader(entry.getKey(), String.valueOf(value));
+          if (!entry.getKey().equalsIgnoreCase(HttpHeaders.CONTENT_LENGTH)) {
+            responseEx.addHeader(entry.getKey(), String.valueOf(value));
+          }
         }
       }
     }
