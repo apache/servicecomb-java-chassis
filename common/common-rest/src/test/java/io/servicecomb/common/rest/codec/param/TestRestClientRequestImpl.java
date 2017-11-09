956e5cdeb413a69ad76b7522bf1a60248932c66b
jeho0815 <jeho0815@email.com>
2017-11-09 22:09:57 +0800

    JAV-493 send form data must use urlcoding and remove null value to send

---
+++ common/common-rest/src/test/java/io/servicecomb/common/rest/codec/param/TestRestClientRequestImpl.java
@@ -0,0 +1,61 @@
+package io.servicecomb.common.rest.codec.param;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.CaseInsensitiveHeaders;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpHeaders;
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
+
+public class TestRestClientRequestImpl {
+  @Mocked
+  HttpClientRequest request;
+
+  @Test
+  public void testForm() throws Exception {
+    new Expectations() {
+      {
+      }
+    };
+    RestClientRequestImpl restClientRequest = new RestClientRequestImpl(request);
+    restClientRequest.addForm("abc", "Hello");
+    restClientRequest.addForm("def", "world");
+    restClientRequest.addForm("ghi", null);
+    Buffer buffer = restClientRequest.getBodyBuffer();
+    Assert.assertEquals(buffer.toString(), "abc=Hello&def=world&");
+  }
+
+  @Test
+  public void testCookie() throws Exception {
+    HttpClientRequest request = new MockUp<HttpClientRequest>() {
+
+      MultiMap map = new CaseInsensitiveHeaders();
+
+      @Mock
+      public HttpClientRequest putHeader(CharSequence key, CharSequence val) {
+        map.add(key, val);
+        return null;
+      }
+
+      @Mock
+      public MultiMap headers() {
+        return map;
+      }
+    }.getMockInstance();
+    RestClientRequestImpl restClientRequest = new RestClientRequestImpl(request);
+    restClientRequest.addCookie("sessionid", "abcdefghijklmnopqrstuvwxyz");
+    restClientRequest.addCookie("region", "china-north");
+    restClientRequest.write(Buffer.buffer("I love servicecomb"));
+    restClientRequest.end();
+    Buffer buffer = restClientRequest.getBodyBuffer();
+    Assert.assertEquals(buffer.toString(), "I love servicecomb");
+    Assert.assertEquals(restClientRequest.request.headers().get(HttpHeaders.COOKIE),
+        "sessionid=abcdefghijklmnopqrstuvwxyz; region=china-north; ");
+  }
+}
