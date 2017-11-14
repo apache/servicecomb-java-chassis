7a864e0247d66dda0b6f557de1c7c8c377eee6f5
wujimin <wujimin@huawei.com>
2017-11-14 08:54:20 +0800

    JAV-358 EdgeInvocation support control versionRule by caller

--- edge/edge-core/src/main/java/io/servicecomb/edge/core/EdgeInvocation.java
+++ edge/edge-core/src/main/java/io/servicecomb/edge/core/EdgeInvocation.java
@@ -49,6 +49,8 @@ public class EdgeInvocation extends AbstractRestInvocation {
 
   protected ReferenceConfig referenceConfig;
 
+  protected String versionRule = DefinitionConst.VERSION_RULE_ALL;
+
   public void init(String microserviceName, RoutingContext context, String path,
       List<HttpServerFilter> httpServerFilters) {
     this.microserviceName = microserviceName;
@@ -105,6 +107,10 @@ public class EdgeInvocation extends AbstractRestInvocation {
     }
   }
 
+  public void setVersionRule(String versionRule) {
+    this.versionRule = versionRule;
+  }
+
   // another possible rule:
   // path is: /msName/version/.....
   // version in path is v1 or v2 and so on
@@ -116,7 +122,7 @@ public class EdgeInvocation extends AbstractRestInvocation {
   protected String chooseVersionRule() {
     // this will use all instance of the microservice
     // and this required all new version compatible to old version
-    return DefinitionConst.VERSION_RULE_ALL;
+    return versionRule;
   }
 
   protected void prepareEdgeInvoke() throws Throwable {
