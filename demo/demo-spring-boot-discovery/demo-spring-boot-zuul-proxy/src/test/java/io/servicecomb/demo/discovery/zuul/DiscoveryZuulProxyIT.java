900adf3ed56380d199c79c76aefdc9f36381c3ad
laijianbin <laijianbin@huawei.com>
2017-11-15 17:32:01 +0800

    0001,test

--- demo/demo-spring-boot-discovery/demo-spring-boot-zuul-proxy/src/test/java/io/servicecomb/demo/discovery/zuul/DiscoveryZuulProxyIT.java
+++ demo/demo-spring-boot-discovery/demo-spring-boot-zuul-proxy/src/test/java/io/servicecomb/demo/discovery/zuul/DiscoveryZuulProxyIT.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+  
 package io.servicecomb.demo.discovery.zuul;
 
 import static org.assertj.core.api.Assertions.assertThat;
