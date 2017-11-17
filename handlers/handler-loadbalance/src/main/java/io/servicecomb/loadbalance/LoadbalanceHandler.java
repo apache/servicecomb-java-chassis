410ef328742186863d793c27a43676a3a660490c
laijianbin <731608306@qq.com>
2017-11-17 08:54:07 +0800

    JAVA-421 use the new style of loadbalance rule invalid (#266)

--- handlers/handler-loadbalance/src/main/java/io/servicecomb/loadbalance/LoadbalanceHandler.java
+++ handlers/handler-loadbalance/src/main/java/io/servicecomb/loadbalance/LoadbalanceHandler.java
@@ -77,6 +77,7 @@ public class LoadbalanceHandler implements Handler {
   private final Object lock = new Object();
 
   private String policy = null;
+  private String strategy = null;
 
 
   public LoadbalanceHandler() {
@@ -88,13 +89,17 @@ public class LoadbalanceHandler implements Handler {
   @Override
   public void handle(Invocation invocation, AsyncResponse asyncResp) throws Exception {
     String p = Configuration.INSTANCE.getPolicy(invocation.getMicroserviceName());
-    if (this.policy != null && !this.policy.equals(p)) {
+    String strategy = Configuration.INSTANCE.getRuleStrategyName(invocation.getMicroserviceName());
+    
+    if (this.policy != null && !this.policy.equals(p) ||
+    		(this.strategy != null && !this.strategy.equals(strategy))) {
       //配置变化，需要重新生成所有的lb实例
       synchronized (lock) {
         loadBalancerMap.clear();
       }
     }
     this.policy = p;
+    this.strategy = strategy;
 
     LoadBalancer loadBalancer = getOrCreateLoadBalancer(invocation);
     // TODO: after all old filter moved to new filter
