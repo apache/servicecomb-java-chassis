7baacb0d81e66e9da73243d50ec457a41957647d
bao.liu <bao.liu@huawei.com>
2017-12-05 20:32:26 +0800

    []make configuration source to CSE config center open source several integration starters to spring cloud depends on this

---
+++ foundations/foundation-config-cc/src/test/java/io/servicecomb/config/client/TestConfigCenterConfig.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.config.client;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.servicecomb.config.ConfigUtil;
+import io.servicecomb.config.client.ConfigCenterConfig;
+
+/**
+ * Created by l00168639 on 2017/8/31.
+ */
+public class TestConfigCenterConfig {
+    @BeforeClass
+    public static void setUpClass() {
+        ConfigCenterConfig.setConcurrentCompositeConfiguration(ConfigUtil.createLocalConfig());
+    }
+    @Test
+    public void getServerUri() {
+        List<String> servers = ConfigCenterConfig.INSTANCE.getServerUri();
+        Assert.assertEquals("https://10.22.87.59:30103", servers.get(0));
+        Assert.assertEquals("https://10.22.87.59:30103", servers.get(1));
+    }
+}
---
+++ foundations/foundation-config-cc/src/test/java/io/servicecomb/config/client/TestParseConfigUtils.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.config.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.servicecomb.config.archaius.sources.ConfigCenterConfigurationSourceImpl;
+import io.servicecomb.config.archaius.sources.ConfigCenterConfigurationSourceImpl.UpdateHandler;
+import io.servicecomb.config.client.ParseConfigUtils;
+import mockit.Deencapsulation;
+
+/**
+ * @author
+ */
+public class TestParseConfigUtils {
+
+    private ConfigCenterConfigurationSourceImpl configCenterSource = new ConfigCenterConfigurationSourceImpl();
+
+    private UpdateHandler uh = configCenterSource.new UpdateHandler();
+
+    private ParseConfigUtils pc = new ParseConfigUtils(uh);
+
+    @Test
+    public void testNotifyItemsChangedNeedRefresh() {
+
+        boolean status = true;
+        Map<String, Object> before = new HashMap<String, Object>();
+        Map<String, Object> after = new HashMap<String, Object>();
+        try {
+            Deencapsulation.invoke(pc, "notifyItemsChangedNeedRefresh", before, after);
+        } catch (Exception e) {
+            status = false;
+        }
+        Assert.assertTrue(status);
+
+        // Check with valid before object
+        status = true;
+        before.put("test", "testValue");
+        try {
+            Deencapsulation.invoke(pc, "notifyItemsChangedNeedRefresh", before, after);
+        } catch (Exception e) {
+            status = false;
+        }
+        Assert.assertTrue(status);
+
+        // Check with valid after object
+        status = true;
+        after.put("test", "testValue2");
+        try {
+            Deencapsulation.invoke(pc, "notifyItemsChangedNeedRefresh", before, after);
+        } catch (Exception e) {
+            status = false;
+        }
+        Assert.assertTrue(status);
+
+        // Check with valid localItems object
+        status = true;
+        try {
+            Deencapsulation.invoke(pc, "notifyItemsChangedNeedRefresh", before, after);
+        } catch (Exception e) {
+            status = false;
+        }
+        Assert.assertTrue(status);
+    }
+}
