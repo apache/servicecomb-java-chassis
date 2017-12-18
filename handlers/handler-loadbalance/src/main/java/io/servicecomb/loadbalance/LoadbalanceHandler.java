e717721e0cf05439252e94492efe4f19057bd4fe
laijianbin <731608306@qq.com>
2017-12-18 15:13:33 +0800

    Update LoadbalanceHandler.java

--- handlers/handler-loadbalance/src/main/java/io/servicecomb/loadbalance/LoadbalanceHandler.java
+++ handlers/handler-loadbalance/src/main/java/io/servicecomb/loadbalance/LoadbalanceHandler.java
@@ -91,7 +91,7 @@ public class LoadbalanceHandler implements Handler {
   public void handle(Invocation invocation, AsyncResponse asyncResp) throws Exception {
     String policy = Configuration.INSTANCE.getPolicy(invocation.getMicroserviceName());
     String strategy = Configuration.INSTANCE.getRuleStrategyName(invocation.getMicroserviceName());
-    boolean result = StringUtils.equals(policy,this.policy) && StringUtils.equals(strategy, this.strategy);
+    boolean isRuleChanged = StringUtils.equals(policy,this.policy) && StringUtils.equals(strategy, this.strategy);
     
     if (!result){
       //配置变化，需要重新生成所有的lb实例
