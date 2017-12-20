add7c56689ae1a07cd99eb3b6e2713942b6591b2
zhengyangyong <yangyong.zheng@huawei.com>
2017-12-20 16:47:12 +0800

    JAV-540 & SCB-85 Upgrade Metrics File Output from 0.5.0 to 0.6.0
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

---
+++ metrics/metrics-sample/metrics-output-file/src/main/java/io/servicecomb/metrics/sample/output/file/MetricsFileOutput.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.metrics.sample.output.file;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MetricsFileOutput {
+
+  private final Map<String, org.slf4j.Logger> allLoggers = new ConcurrentHashMap<>();
+
+  public MetricsFileOutput() {
+  }
+
+  public void output(Map<String, String> metrics) {
+    for (String metricName : metrics.keySet()) {
+      org.slf4j.Logger logger = allLoggers.computeIfAbsent(metricName, l -> LoggerFactory.getLogger(metricName));
+      logger.error(metrics.get(metricName));
+    }
+  }
+}
---
+++ metrics/metrics-sample/metrics-output-file/src/main/java/io/servicecomb/metrics/sample/output/file/MetricsObserverInitializer.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.metrics.sample.output.file;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.netflix.config.DynamicPropertyFactory;
+
+import io.servicecomb.metrics.core.metric.RegistryMetric;
+import io.servicecomb.metrics.core.publish.DataSource;
+
+@Component
+public class MetricsObserverInitializer {
+
+  public static final String METRICS_POLL_TIME = "servicecomb.metrics.polltime";
+
+  public static final String METRICS_FILE_ENABLED = "servicecomb.metrics.file.enabled";
+
+  private final int metricPoll;
+
+  private final MetricsFileOutput fileOutput;
+
+  private final MetricsContentConvertor convertor;
+
+  private final MetricsContentFormatter formatter;
+
+  private final DataSource dataSource;
+
+  @Autowired
+  public MetricsObserverInitializer(MetricsFileOutput fileOutput, MetricsContentConvertor convertor,
+      MetricsContentFormatter formatter, DataSource dataSource) {
+    metricPoll = DynamicPropertyFactory.getInstance().getIntProperty(METRICS_POLL_TIME, 30).get();
+    this.fileOutput = fileOutput;
+    this.convertor = convertor;
+    this.formatter = formatter;
+    this.dataSource = dataSource;
+    this.init();
+  }
+
+  public void init() {
+    if (isRollingFileEnabled()) {
+      final Runnable poller = this::run;
+      Executors.newScheduledThreadPool(1)
+          .scheduleWithFixedDelay(poller, 0, metricPoll, SECONDS);
+    }
+  }
+
+  private void run() {
+    RegistryMetric registryMetric = dataSource.getRegistryMetric(0);
+    Map<String, String> convertedMetrics = convertor.convert(registryMetric);
+    Map<String, String> formattedMetrics = formatter.format(convertedMetrics);
+    fileOutput.output(formattedMetrics);
+  }
+
+  public boolean isRollingFileEnabled() {
+    return DynamicPropertyFactory.getInstance().getBooleanProperty(METRICS_FILE_ENABLED, false).get();
+  }
+}
