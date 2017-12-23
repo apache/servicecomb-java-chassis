7a1cddb4f91078668ebf1f0f6d005de4ce05e3cb
zhengyangyong <yangyong.zheng@huawei.com>
2017-12-23 10:27:59 +0800

    JAV-539 & SCB-9 add code comment for windowtime
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- metrics/metrics-core/src/main/java/io/servicecomb/metrics/core/publish/DataSource.java
+++ metrics/metrics-core/src/main/java/io/servicecomb/metrics/core/publish/DataSource.java
@@ -22,9 +22,41 @@ import java.util.List;
 import io.servicecomb.metrics.core.metric.RegistryMetric;
 
 public interface DataSource {
+
+  /**  What's the WindowTime ?
+  We all know there are two major type of metric :
+   1.Time-unrelated,you can get the latest value any time immediately:
+   Counter -> increase or decrease
+   Guage -> set a certain one value
+   2.Time-related,only after a centain time pass you can compute the right value,"a centain time" called WindowTime
+   Max & Min -> the max value or min value in a centain time
+   Average -> average value, the simplest algorithm is f = sum / count
+   Rate -> like TPS,algorithm is f = sum / second
+
+   Will be return "servicecomb.metrics.window_time" setting in microservice.yaml
+  */
+  List<Long> getAppliedWindowTime();
+
+  //return getRegistryMetric(0)
   RegistryMetric getRegistryMetric();
 
+  /**
+   * windowTimeIndex usage example:
+   * if there is two window time set in "servicecomb.metrics.window_time" like 1000,2000
+   * then windowTimeIndex = 0 will return result of the setting 1000(1 second)
+   * windowTimeIndex = 1 will return result of the setting 2000(2 second)
+   *
+   * there are three monitor of max,min,total
+   * 0----------1----------2----------            <-time line (second)
+   *   100,200    300,400                         <-value record
+   *
+   *                 ↑ getRegistryMetric(0) will return max=200 min=100 total=300
+   *                   getRegistryMetric(1) will return max=0 min=0 total=0
+   *                             ↑ getRegistryMetric(0) will return max=300 min=400 total=700
+   *                               getRegistryMetric(1) will return max=400 min=100 total=1000
+   *
+   * @param windowTimeIndex index of getAppliedWindowTime() item
+   * @return RegistryMetric
+   */
   RegistryMetric getRegistryMetric(int windowTimeIndex);
-
-  List<Long> getAppliedWindowTime();
 }
