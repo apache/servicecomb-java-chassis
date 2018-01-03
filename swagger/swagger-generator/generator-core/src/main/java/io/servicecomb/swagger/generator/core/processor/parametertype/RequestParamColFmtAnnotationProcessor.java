61211fe8a1d5bacc6be7c2b3f3f6311f8876abc7
wWX495170 <weichao16@huawei.com>
2018-01-03 19:23:05 +0800

    JAV-579 support dataStrArray

---
+++ swagger/swagger-generator/generator-core/src/main/java/io/servicecomb/swagger/generator/core/processor/parametertype/RequestParamColFmtAnnotationProcessor.java
@@ -0,0 +1,25 @@
+package io.servicecomb.swagger.generator.core.processor.parametertype;
+
+import io.servicecomb.swagger.extend.annotations.RequestParamColFmt;
+import io.servicecomb.swagger.generator.core.OperationGenerator;
+import io.servicecomb.swagger.generator.core.processor.parameter.AbstractParameterProcessor;
+import io.swagger.models.parameters.QueryParameter;
+
+public class RequestParamColFmtAnnotationProcessor extends AbstractParameterProcessor<QueryParameter> {
+  @Override
+  protected QueryParameter createParameter() {
+    return null;
+  }
+
+  @Override
+  protected String getAnnotationParameterName(Object annotation) {
+    return null;
+  }
+
+  protected void fillParameter(Object annotation, OperationGenerator operationGenerator, int paramIdx,QueryParameter parameter){
+    setParameterName(annotation, operationGenerator, paramIdx, parameter);
+    setParameterType(operationGenerator, paramIdx, parameter);
+    String collectionFormat = ((RequestParamColFmt) annotation).collectionFormat();
+    parameter.setCollectionFormat(collectionFormat);
+  }
+}
