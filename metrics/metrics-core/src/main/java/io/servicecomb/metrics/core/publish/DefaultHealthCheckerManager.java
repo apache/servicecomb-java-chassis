4eaf6cda32717c07d29fa210593859989e95f69e
zhengyangyong <yangyong.zheng@huawei.com>
2018-01-09 16:37:57 +0800

    SCB-166 rebase and fix pr comment
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- metrics/metrics-core/src/main/java/io/servicecomb/metrics/core/publish/DefaultHealthCheckerManager.java
+++ metrics/metrics-core/src/main/java/io/servicecomb/metrics/core/publish/DefaultHealthCheckerManager.java
@@ -19,6 +19,7 @@ package io.servicecomb.metrics.core.publish;
 
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
@@ -27,7 +28,6 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import io.servicecomb.metrics.common.HealthCheckResult;
@@ -42,20 +42,15 @@ public class DefaultHealthCheckerManager implements HealthCheckerManager {
 
   private final Map<String, HealthChecker> healthCheckers;
 
-  private ApplicationContext context;
-
-  @Autowired
-  public DefaultHealthCheckerManager(ApplicationContext context) {
-    this.context = context;
+  @Autowired(required = false)
+  public DefaultHealthCheckerManager(List<HealthChecker> springHealthCheckers) {
     this.healthCheckers = new ConcurrentHashMap<>();
     HealthChecker defaultHealthChecker = new DefaultMicroserviceHealthChecker();
     this.healthCheckers.put(defaultHealthChecker.getName(), defaultHealthChecker);
 
-    for (String beanName : context.getBeanDefinitionNames()) {
-      Class<?> beanClass = context.getType(beanName);
-      if (HealthChecker.class.isAssignableFrom(beanClass)) {
-        Object bean = context.getBean(beanName);
-        this.healthCheckers.put(((HealthChecker) bean).getName(), (HealthChecker) bean);
+    if (springHealthCheckers != null && !springHealthCheckers.isEmpty()) {
+      for (HealthChecker checker : springHealthCheckers) {
+        this.healthCheckers.put(checker.getName(), checker);
       }
     }
   }
