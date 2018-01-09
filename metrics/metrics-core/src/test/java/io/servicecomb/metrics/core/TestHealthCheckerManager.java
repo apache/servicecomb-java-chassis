db62107d6e10ab989926967518288ade4db9e9f2
zhengyangyong <yangyong.zheng@huawei.com>
2018-01-09 16:37:57 +0800

    SCB-166 rebase and fix pr comment
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- metrics/metrics-core/src/test/java/io/servicecomb/metrics/core/TestHealthCheckerManager.java
+++ metrics/metrics-core/src/test/java/io/servicecomb/metrics/core/TestHealthCheckerManager.java
@@ -18,29 +18,19 @@
 package io.servicecomb.metrics.core;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.context.NoSuchMessageException;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
 
 import com.google.common.collect.Lists;
 
 import io.servicecomb.foundation.common.utils.JsonUtils;
 import io.servicecomb.metrics.common.DefaultHealthCheckExtraData;
 import io.servicecomb.metrics.common.HealthCheckResult;
+import io.servicecomb.metrics.common.HealthChecker;
 import io.servicecomb.metrics.core.publish.DefaultHealthCheckerManager;
 import io.servicecomb.metrics.core.publish.HealthCheckerManager;
 import io.servicecomb.serviceregistry.RegistryUtils;
@@ -77,209 +67,20 @@ public class TestHealthCheckerManager {
       }
     };
 
-    HealthCheckerManager manager = new DefaultHealthCheckerManager(new ApplicationContext() {
+    List<HealthChecker> checkers = new ArrayList<>();
+    checkers.add(new HealthChecker() {
       @Override
-      public String getId() {
-        return null;
+      public String getName() {
+        return "custom";
       }
 
       @Override
-      public String getApplicationName() {
-        return null;
-      }
-
-      @Override
-      public String getDisplayName() {
-        return null;
-      }
-
-      @Override
-      public long getStartupDate() {
-        return 0;
-      }
-
-      @Override
-      public ApplicationContext getParent() {
-        return null;
-      }
-
-      @Override
-      public AutowireCapableBeanFactory getAutowireCapableBeanFactory() throws IllegalStateException {
-        return null;
-      }
-
-      @Override
-      public BeanFactory getParentBeanFactory() {
-        return null;
-      }
-
-      @Override
-      public boolean containsLocalBean(String s) {
-        return false;
-      }
-
-      @Override
-      public boolean containsBeanDefinition(String s) {
-        return false;
-      }
-
-      @Override
-      public int getBeanDefinitionCount() {
-        return 0;
-      }
-
-      @Override
-      public String[] getBeanDefinitionNames() {
-        return new String[0];
-      }
-
-      @Override
-      public String[] getBeanNamesForType(ResolvableType resolvableType) {
-        return new String[0];
-      }
-
-      @Override
-      public String[] getBeanNamesForType(Class<?> aClass) {
-        return new String[0];
-      }
-
-      @Override
-      public String[] getBeanNamesForType(Class<?> aClass, boolean b, boolean b1) {
-        return new String[0];
-      }
-
-      @Override
-      public <T> Map<String, T> getBeansOfType(Class<T> aClass) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public <T> Map<String, T> getBeansOfType(Class<T> aClass, boolean b, boolean b1) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public String[] getBeanNamesForAnnotation(Class<? extends Annotation> aClass) {
-        return new String[0];
-      }
-
-      @Override
-      public Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> aClass) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public <A extends Annotation> A findAnnotationOnBean(String s, Class<A> aClass)
-          throws NoSuchBeanDefinitionException {
-        return null;
-      }
-
-      @Override
-      public Object getBean(String s) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public <T> T getBean(String s, Class<T> aClass) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public <T> T getBean(Class<T> aClass) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public Object getBean(String s, Object... objects) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public <T> T getBean(Class<T> aClass, Object... objects) throws BeansException {
-        return null;
-      }
-
-      @Override
-      public boolean containsBean(String s) {
-        return false;
-      }
-
-      @Override
-      public boolean isSingleton(String s) throws NoSuchBeanDefinitionException {
-        return false;
-      }
-
-      @Override
-      public boolean isPrototype(String s) throws NoSuchBeanDefinitionException {
-        return false;
-      }
-
-      @Override
-      public boolean isTypeMatch(String s, ResolvableType resolvableType) throws NoSuchBeanDefinitionException {
-        return false;
-      }
-
-      @Override
-      public boolean isTypeMatch(String s, Class<?> aClass) throws NoSuchBeanDefinitionException {
-        return false;
-      }
-
-      @Override
-      public Class<?> getType(String s) throws NoSuchBeanDefinitionException {
-        return null;
-      }
-
-      @Override
-      public String[] getAliases(String s) {
-        return new String[0];
-      }
-
-      @Override
-      public void publishEvent(ApplicationEvent applicationEvent) {
-
-      }
-
-      @Override
-      public void publishEvent(Object o) {
-
-      }
-
-      @Override
-      public String getMessage(String s, Object[] objects, String s1, Locale locale) {
-        return null;
-      }
-
-      @Override
-      public String getMessage(String s, Object[] objects, Locale locale) throws NoSuchMessageException {
-        return null;
-      }
-
-      @Override
-      public String getMessage(MessageSourceResolvable messageSourceResolvable, Locale locale)
-          throws NoSuchMessageException {
-        return null;
-      }
-
-      @Override
-      public Environment getEnvironment() {
-        return null;
-      }
-
-      @Override
-      public Resource[] getResources(String s) throws IOException {
-        return new Resource[0];
-      }
-
-      @Override
-      public Resource getResource(String s) {
-        return null;
-      }
-
-      @Override
-      public ClassLoader getClassLoader() {
-        return null;
+      public HealthCheckResult check() {
+        return new HealthCheckResult(true, "", "");
       }
     });
+
+    HealthCheckerManager manager = new DefaultHealthCheckerManager(checkers);
     Map<String, HealthCheckResult> results = manager.check();
 
     Assert.assertTrue(results.get("default").isHealthy());
