1a151ac33bd235622cf3d15a2bebbd739c43b936
zhengyangyong <yangyong.zheng@huawei.com>
2018-01-11 14:14:09 +0800

    SCB-150 add success/failed dimension for call count and tps
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- metrics/metrics-core/src/main/java/org/apache/servicecomb/metrics/core/monitor/CallMonitor.java
+++ metrics/metrics-core/src/main/java/org/apache/servicecomb/metrics/core/monitor/CallMonitor.java
@@ -19,37 +19,82 @@ package org.apache.servicecomb.metrics.core.monitor;
 
 import org.apache.servicecomb.metrics.common.CallMetric;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.netflix.servo.monitor.BasicCounter;
 import com.netflix.servo.monitor.MonitorConfig;
 import com.netflix.servo.monitor.StepCounter;
+import com.netflix.servo.tag.Tag;
+import com.netflix.servo.tag.TagList;
+
+import io.servicecomb.metrics.common.CallMetric;
+import io.servicecomb.metrics.common.DoubleMetricValue;
+import io.servicecomb.metrics.common.LongMetricValue;
+import io.servicecomb.metrics.core.MetricsDimension;
 
 public class CallMonitor {
   private final String prefix;
 
-  private final BasicCounter total;
+  private final List<BasicCounter> totalCounters;
 
-  private final StepCounter tps;
+  private final List<StepCounter> tpsCounters;
 
-  public CallMonitor(String prefix) {
+  public CallMonitor(String prefix, String... dimensions) {
     this.prefix = prefix;
-    this.total = new BasicCounter(MonitorConfig.builder(prefix + ".total").build());
-    this.tps = new StepCounter(MonitorConfig.builder(prefix + ".tps").build());
+    this.totalCounters = new ArrayList<>();
+    this.tpsCounters = new ArrayList<>();
+    if (dimensions.length == 0) {
+      this.totalCounters.add(new BasicCounter(MonitorConfig.builder(prefix + ".total").build()));
+      this.tpsCounters.add(new StepCounter(MonitorConfig.builder(prefix + ".tps").build()));
+    } else {
+      for (String dimension : dimensions) {
+        for (String option : MetricsDimension.getDimensionOptions(dimension)) {
+          this.totalCounters
+              .add(new BasicCounter(MonitorConfig.builder(prefix + ".total").withTag(dimension, option).build()));
+          this.tpsCounters
+              .add(new StepCounter(MonitorConfig.builder(prefix + ".tps").withTag(dimension, option).build()));
+        }
+      }
+    }
   }
 
   public void increment() {
-    total.increment();
-    tps.increment();
+    for (int i = 0; i < totalCounters.size(); i++) {
+      totalCounters.get(i).increment();
+      tpsCounters.get(i).increment();
+    }
   }
 
   public CallMetric toMetric(int windowTimeIndex) {
-    return new CallMetric(this.prefix, total.getValue(windowTimeIndex).longValue(),
-        this.adjustValue(tps.getValue(windowTimeIndex).doubleValue()));
+    List<LongMetricValue> totalValues = new ArrayList<>();
+    List<DoubleMetricValue> tpsValues = new ArrayList<>();
+    for (int i = 0; i < totalCounters.size(); i++) {
+      totalValues.add(new LongMetricValue(totalCounters.get(i).getValue(windowTimeIndex).longValue(),
+          convertTags(totalCounters.get(i).getConfig().getTags())));
+      tpsValues.add(new DoubleMetricValue(adjustValue(tpsCounters.get(i).getValue(windowTimeIndex).doubleValue()),
+          convertTags(tpsCounters.get(i).getConfig().getTags())));
+    }
+    return new CallMetric(this.prefix, totalValues, tpsValues);
   }
 
   //for time-related monitor type, if stop poll value over one window time,
   //the value may return -1 because servo can't known precise value of previous step
   //so must change to return 0
-  public double adjustValue(double value) {
+  private double adjustValue(double value) {
     return value < 0 ? 0 : value;
+  }
+
+  private Map<String, String> convertTags(TagList tags) {
+    if (tags.size() != 0) {
+      Map<String, String> tagMap = new HashMap<>();
+      for (Tag tag : tags) {
+        tagMap.put(tag.getKey(), tag.getValue());
+      }
+      return tagMap;
+    }
+    return null;
   }
 }
