b7c8b175bcb54a20ff29e8649d7b56aa301e1fd2
wujimin <wujimin@huawei.com>
2018-01-26 16:58:48 +0800

    SCB-310 rearrange perf metrics output and add eventloopContext created count output

--- demo/perf/src/main/java/org/apache/servicecomb/demo/perf/PerfMetricsFilePublisher.java
+++ demo/perf/src/main/java/org/apache/servicecomb/demo/perf/PerfMetricsFilePublisher.java
@@ -18,6 +18,7 @@ package org.apache.servicecomb.demo.perf;
 
 import java.util.Map.Entry;
 
+import org.apache.servicecomb.foundation.vertx.VertxUtils;
 import org.apache.servicecomb.metrics.common.ConsumerInvocationMetric;
 import org.apache.servicecomb.metrics.common.MetricsDimension;
 import org.apache.servicecomb.metrics.common.ProducerInvocationMetric;
@@ -25,6 +26,8 @@ import org.apache.servicecomb.metrics.common.RegistryMetric;
 import org.apache.servicecomb.metrics.core.publish.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.vertx.core.impl.VertxImplEx;
 
 public class PerfMetricsFilePublisher {
   private static final Logger LOGGER = LoggerFactory.getLogger(PerfMetricsFilePublisher.class);
@@ -41,6 +44,15 @@ public class PerfMetricsFilePublisher {
     StringBuilder sb = new StringBuilder();
     sb.append("\n");
 
+    collectSystemMetrics(metric, sb);
+    collectVertxMetrics(metric, sb);
+    collectConsumerMetrics(metric, sb);
+    collectProducerMetrics(metric, sb);
+
+    LOGGER.info(sb.toString());
+  }
+
+  protected void collectSystemMetrics(RegistryMetric metric, StringBuilder sb) {
     double cpu = metric.getInstanceMetric().getSystemMetric().getCpuLoad();
     // can not get cpu usage in windows, so skip this information
     if (cpu >= 0) {
@@ -48,41 +60,67 @@ public class PerfMetricsFilePublisher {
           .append((long) cpu * Runtime.getRuntime().availableProcessors())
           .append("%\n");
     }
+  }
 
-    sb.append("consumer:\n"
-        + "  total           tps             latency(ms)     name\n");
-    for (Entry<String, ConsumerInvocationMetric> entry : metric.getConsumerMetrics().entrySet()) {
-      String opName = entry.getKey();
-      sb.append(String
-          .format("  %-16d%-16d%-16.3f%s\n",
-              entry.getValue().getConsumerCall()
-                  .getTotalValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL).getValue(),
-              entry.getValue().getConsumerCall()
-                  .getTpsValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL).getValue()
-                  .longValue(),
-              entry.getValue().getConsumerLatency().getAverage(),
-              opName));
+  protected void collectVertxMetrics(RegistryMetric metric, StringBuilder sb) {
+    sb.append("vertx:\n")
+        .append("  name       eventLoopContext-created\n");
+    for (Entry<String, VertxImplEx> entry : VertxUtils.getVertxMap().entrySet()) {
+      sb.append(String.format("  %-10s %-19d\n",
+          entry.getKey(),
+          entry.getValue().getEventLoopContextCreatedCount()));
+    }
+  }
+
+  protected void collectProducerMetrics(RegistryMetric metric, StringBuilder sb) {
+    if (metric.getProducerMetrics().isEmpty()) {
+      return;
     }
 
     sb.append("producer:\n"
-        + "  total           tps             latency(ms)     queue(ms)       execute(ms)     name\n");
+        + "  total               tps     latency(ms) queue(ms) execute(ms) name\n");
     for (Entry<String, ProducerInvocationMetric> entry : metric.getProducerMetrics().entrySet()) {
       String opName = entry.getKey();
       sb.append(
-          String.format("  %-16d%-16d%-16.3f%-16.3f%-16.3f%s\n",
-              entry.getValue().getProducerCall()
-                  .getTotalValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL).getValue(),
-              entry.getValue().getProducerCall()
-                  .getTpsValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL).getValue()
+          String.format("  %-19d %-7d %-11.3f %-9.3f %-11.3f %s\n",
+              entry.getValue()
+                  .getProducerCall()
+                  .getTotalValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL)
+                  .getValue(),
+              entry.getValue()
+                  .getProducerCall()
+                  .getTpsValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL)
+                  .getValue()
                   .longValue(),
               entry.getValue().getProducerLatency().getAverage(),
               entry.getValue().getLifeTimeInQueue().getAverage(),
               entry.getValue().getExecutionTime().getAverage(),
               opName));
     }
+  }
 
-    sb.setLength(sb.length() - 1);
+  protected void collectConsumerMetrics(RegistryMetric metric, StringBuilder sb) {
+    if (metric.getConsumerMetrics().isEmpty()) {
+      return;
+    }
 
-    LOGGER.info(sb.toString());
+    sb.append("consumer:\n"
+        + "  total               tps     latency(ms) name\n");
+    for (Entry<String, ConsumerInvocationMetric> entry : metric.getConsumerMetrics().entrySet()) {
+      String opName = entry.getKey();
+      sb.append(String
+          .format("  %-19d %-7d %-11.3f %s\n",
+              entry.getValue()
+                  .getConsumerCall()
+                  .getTotalValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL)
+                  .getValue(),
+              entry.getValue()
+                  .getConsumerCall()
+                  .getTpsValue(MetricsDimension.DIMENSION_STATUS, MetricsDimension.DIMENSION_STATUS_ALL)
+                  .getValue()
+                  .longValue(),
+              entry.getValue().getConsumerLatency().getAverage(),
+              opName));
+    }
   }
 }
