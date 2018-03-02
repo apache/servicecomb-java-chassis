59e0eecc86612f88525b5446871a1245e5c9dc2e
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-03-02 13:45:09 +0800

    Update the Encryptions for Keys

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -43,7 +43,7 @@ if [ "$1" == "install" ]; then
 else
         if [ "$TAGGEDCOMMIT" ==   "true" ]; then
                 echo "Decrypting the key"
-		openssl aes-256-cbc -K $encrypted_acbbc88fb3ab_key -iv $encrypted_acbbc88fb3ab_iv -in gpg-sec.tar.enc -out gpg-sec.tar -d
+		openssl aes-256-cbc -K $encrypted_c02fb37685d7_key -iv $encrypted_c02fb37685d7_iv -in gpg-sec.tar.enc -out gpg-sec.tar -d
 		tar xvf gpg-sec.tar
 		echo "Deploying Staging Release"
 		mvn deploy -DskipTests -Prelease -Pdistribution -Ppassphrase --settings .travis.settings.xml
