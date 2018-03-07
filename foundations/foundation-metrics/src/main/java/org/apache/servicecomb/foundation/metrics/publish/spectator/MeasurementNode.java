7174011961d1efc7f9ebc9a3193f1bf38f6813b2
wujimin <wujimin@huawei.com>
2018-03-07 16:57:05 +0800

    SCB-369 extract informations from spectator measurement, just like sql: select * from meters group by ......, but output is a tree not a table

---
+++ foundations/foundation-metrics/src/main/java/org/apache/servicecomb/foundation/metrics/publish/spectator/MeasurementNode.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicecomb.foundation.metrics.publish.spectator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.netflix.spectator.api.Measurement;
+
+public class MeasurementNode {
+  private String name;
+
+  private List<Measurement> measurements = new ArrayList<>();
+
+  private Map<String, MeasurementNode> children;
+
+  public MeasurementNode(String name, Map<String, MeasurementNode> children) {
+    this.name = name;
+    this.children = children;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Map<String, MeasurementNode> getChildren() {
+    return children;
+  }
+
+  public MeasurementNode findChild(String childName) {
+    if (children == null) {
+      return null;
+    }
+    return children.get(childName);
+  }
+
+  public MeasurementNode findChild(String... childNames) {
+    MeasurementNode node = this;
+    for (String childName : childNames) {
+      if (node == null) {
+        return null;
+      }
+
+      node = node.findChild(childName);
+    }
+    return node;
+  }
+
+  public MeasurementNode addChild(String childName, Measurement measurement) {
+    if (children == null) {
+      children = new HashMap<>();
+    }
+
+    MeasurementNode node = children.computeIfAbsent(childName, name -> {
+      return new MeasurementNode(name, null);
+    });
+    node.addMeasurement(measurement);
+
+    return node;
+  }
+
+  public List<Measurement> getMeasurements() {
+    return measurements;
+  }
+
+  public void addMeasurement(Measurement measurement) {
+    measurements.add(measurement);
+  }
+}
---
+++ foundations/foundation-metrics/src/test/java/org/apache/servicecomb/foundation/metrics/publish/spectator/TestMeasurementNode.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicecomb.foundation.metrics.publish.spectator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.netflix.spectator.api.Measurement;
+
+import mockit.Mocked;
+
+public class TestMeasurementNode {
+  MeasurementNode node = new MeasurementNode("name", null);
+
+  @Test
+  public void getName() {
+    Assert.assertEquals("name", node.getName());
+  }
+
+  @Test
+  public void getChildren() {
+    Map<String, MeasurementNode> children = new HashMap<>();
+    node = new MeasurementNode("name", children);
+
+    Assert.assertSame(children, node.getChildren());
+  }
+
+  @Test
+  public void findChild_noChildren() {
+    Assert.assertNull(node.findChild("child"));
+  }
+
+  @Test
+  public void findChild_multiLevel_noMiddleChildren(@Mocked Measurement measurement) {
+    MeasurementNode c1 = node.addChild("c1", measurement);
+    c1.addChild("c2", measurement);
+
+    Assert.assertNull(node.findChild("c1_notExist", "c2"));
+  }
+
+  @Test
+  public void findChild_multiLevel_ok(@Mocked Measurement measurement) {
+    MeasurementNode c1 = node.addChild("c1", measurement);
+    MeasurementNode c2 = c1.addChild("c2", measurement);
+
+    Assert.assertSame(c2, node.findChild("c1", "c2"));
+  }
+
+  @Test
+  public void addChild(@Mocked Measurement measurement) {
+    MeasurementNode c1 = node.addChild("c1", measurement);
+    MeasurementNode c2 = node.addChild("c2", measurement);
+
+    Assert.assertSame(c1, node.findChild("c1"));
+    Assert.assertSame(c2, node.findChild("c2"));
+  }
+
+  @Test
+  public void getMeasurements(@Mocked Measurement measurement) {
+    node.addMeasurement(measurement);
+
+    Assert.assertThat(node.getMeasurements(), Matchers.contains(measurement));
+  }
+}
