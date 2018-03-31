3e9e29ed3b6947e6e5c6c38bfcb98ca5c7433c46
wujimin <wujimin@huawei.com>
2018-03-31 08:36:05 +0800

    SCB-385 register publisher schemas to core

---
+++ metrics/metrics-core/src/test/java/org/apache/servicecomb/metrics/core/TestMetricsBootListener.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicecomb.metrics.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.servicecomb.core.BootListener.BootEvent;
+import org.apache.servicecomb.core.BootListener.EventType;
+import org.apache.servicecomb.core.definition.SchemaMeta;
+import org.apache.servicecomb.core.definition.schema.ProducerSchemaFactory;
+import org.apache.servicecomb.foundation.common.utils.SPIServiceUtils;
+import org.apache.servicecomb.foundation.metrics.MetricsInitializer;
+import org.apache.servicecomb.metrics.core.publish.HealthCheckerRestPublisher;
+import org.apache.servicecomb.metrics.core.publish.MetricsRestPublisher;
+import org.apache.servicecomb.serviceregistry.RegistryUtils;
+import org.apache.servicecomb.serviceregistry.api.registry.Microservice;
+import org.junit.Assert;
+import org.junit.Test;
+
+import mockit.Deencapsulation;
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
+
+public class TestMetricsBootListener {
+  MetricsBootListener listener = new MetricsBootListener();
+
+  @Test
+  public void registerSchemas() {
+    List<Object[]> argsList = new ArrayList<>();
+
+    ProducerSchemaFactory producerSchemaFactory = new MockUp<ProducerSchemaFactory>() {
+      @Mock
+      SchemaMeta getOrCreateProducerSchema(String microserviceName, String schemaId,
+          Class<?> producerClass,
+          Object producerInstance) {
+        argsList.add(new Object[] {microserviceName, schemaId, producerClass, producerInstance});
+        return null;
+      }
+    }.getMockInstance();
+    Deencapsulation.setField(listener, "producerSchemaFactory", producerSchemaFactory);
+
+    Microservice microservice = new Microservice();
+    microservice.setServiceName("name");
+    new Expectations(RegistryUtils.class) {
+      {
+        RegistryUtils.getMicroservice();
+        result = microservice;
+      }
+    };
+
+    BootEvent event = new BootEvent();
+    event.setEventType(EventType.BEFORE_PRODUCER_PROVIDER);
+    listener.onBootEvent(event);
+
+    Object[] args = argsList.get(0);
+    Assert.assertEquals("name", args[0]);
+    Assert.assertEquals("healthEndpoint", args[1]);
+    Assert.assertEquals(HealthCheckerRestPublisher.class, args[2]);
+    Assert.assertEquals(HealthCheckerRestPublisher.class, args[3].getClass());
+
+    MetricsRestPublisher metricsRestPublisher =
+        SPIServiceUtils.getTargetService(MetricsInitializer.class, MetricsRestPublisher.class);
+    args = argsList.get(1);
+    Assert.assertEquals("name", args[0]);
+    Assert.assertEquals("metricsEndpoint", args[1]);
+    Assert.assertEquals(MetricsRestPublisher.class, args[2]);
+    Assert.assertSame(metricsRestPublisher, args[3]);
+  }
+}
