702e18d6fa7a8bcad97b44b5ab07f708797726be
wujimin <wujimin@huawei.com>
2018-04-03 18:26:19 +0800

    SCB-422 prometheus switch to new mechanism

--- demo/demo-springmvc/springmvc-client/src/main/java/org/apache/servicecomb/demo/springmvc/client/SpringmvcClient.java
+++ demo/demo-springmvc/springmvc-client/src/main/java/org/apache/servicecomb/demo/springmvc/client/SpringmvcClient.java
@@ -109,13 +109,13 @@ public class SpringmvcClient {
     try {
       String content = restTemplate.getForObject("cse://springmvc/codeFirstSpringmvc/prometheusForTest", String.class);
 
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_addDate"));
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_sayHello"));
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_fallbackFromCache"));
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_isTrue"));
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_add"));
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_sayHi2"));
-      TestMgr.check(true, content.contains("servicecomb_invocation_springmvc_codeFirst_saySomething"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.addDate"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.sayHello"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.fallbackFromCache"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.isTrue"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.add"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.sayHi2"));
+      TestMgr.check(true, content.contains("servicecomb_invocation{operation=\"springmvc.codeFirst.saySomething"));
 
       String[] metricLines = content.split("\n");
       if (metricLines.length > 0) {
