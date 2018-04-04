7b279e35944641d6ca6029cd5174dc807f0f0ca4
wujimin <wujimin@huawei.com>
2018-04-04 09:05:33 +0800

    SCB-422 split create registry and create invocation meters to different initializer

--- metrics/metrics-core/src/main/java/org/apache/servicecomb/metrics/core/InvocationMetersInitializer.java
+++ metrics/metrics-core/src/main/java/org/apache/servicecomb/metrics/core/InvocationMetersInitializer.java
@@ -19,6 +19,7 @@ package org.apache.servicecomb.metrics.core;
 import org.apache.servicecomb.core.Invocation;
 import org.apache.servicecomb.core.event.InvocationFinishEvent;
 import org.apache.servicecomb.core.event.InvocationStartEvent;
+import org.apache.servicecomb.foundation.common.utils.SPIServiceUtils;
 import org.apache.servicecomb.foundation.metrics.MetricsBootstrapConfig;
 import org.apache.servicecomb.foundation.metrics.MetricsInitializer;
 import org.apache.servicecomb.metrics.core.meter.ConsumerMeters;
@@ -30,33 +31,22 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.netflix.spectator.api.CompositeRegistry;
 import com.netflix.spectator.api.Registry;
-import com.netflix.spectator.servo.ServoRegistry;
 
-public class DefaultMetricsInitializer implements MetricsInitializer {
-  public static final String METRICS_WINDOW_TIME = "servicecomb.metrics.window_time";
-
-  public static final int DEFAULT_METRICS_WINDOW_TIME = 5000;
-
-  private Registry registry;
-
+public class InvocationMetersInitializer implements MetricsInitializer {
   private ConsumerMeters consumerMeters;
 
   private ProducerMeters producerMeters;
 
   @Override
   public void init(CompositeRegistry globalRegistry, EventBus eventBus, MetricsBootstrapConfig config) {
-    registry = createRegistry(config);
+    DefaultRegistryInitializer defaultRegistryInitializer =
+        SPIServiceUtils.getTargetService(MetricsInitializer.class, DefaultRegistryInitializer.class);
+    Registry registry = defaultRegistryInitializer.getRegistry();
 
-    this.consumerMeters = new ConsumerMeters(registry);
-    this.producerMeters = new ProducerMeters(registry);
+    consumerMeters = new ConsumerMeters(registry);
+    producerMeters = new ProducerMeters(registry);
 
-    globalRegistry.add(registry);
     eventBus.register(this);
-  }
-
-  protected Registry createRegistry(MetricsBootstrapConfig config) {
-    System.getProperties().setProperty("servo.pollers", String.valueOf(config.getMsPollInterval()));
-    return new ServoRegistry();
   }
 
   protected AbstractInvocationMeters findInvocationMeters(Invocation invocation) {
