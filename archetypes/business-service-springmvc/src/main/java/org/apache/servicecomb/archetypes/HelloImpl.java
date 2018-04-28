fed0fea55622e613bf860cf2be36d6320f090924
zhengyangyong <yangyong.zheng@huawei.com>
2018-04-28 10:36:51 +0800

    SCB-527 adjust maven structure and fix some mistake
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- archetypes/business-service-springmvc/src/main/java/org/apache/servicecomb/archetypes/HelloImpl.java
+++ archetypes/business-service-springmvc/src/main/java/org/apache/servicecomb/archetypes/HelloImpl.java
@@ -21,9 +21,9 @@ import org.apache.servicecomb.provider.rest.common.RestSchema;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestSchema(schemaId = "helloEndpoint")
+@RestSchema(schemaId = "hello")
 @RequestMapping(path = "/")
-public class HelloEndpoint {
+public class HelloImpl {
 
   @GetMapping(path = "/hello")
   public String hello() {
