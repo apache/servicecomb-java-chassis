047d65d4c4fd0bc4911877ed8c86cd22f707d522
wujimin <wujimin@huawei.com>
2018-06-04 00:12:06 +0800

    [SCB-637] support getCharsetFromContentType in HttpUtils

--- foundations/foundation-common/src/main/java/org/apache/servicecomb/foundation/common/http/HttpUtils.java
+++ foundations/foundation-common/src/main/java/org/apache/servicecomb/foundation/common/http/HttpUtils.java
@@ -81,4 +81,32 @@ public final class HttpUtils {
     fileName = uriDecodePath(fileName);
     return new File(fileName).getName();
   }
+
+  /**
+   * Parse the character encoding from the specified content type header.
+   * If the content type is null, or there is no explicit character encoding,
+   * <code>null</code> is returned.
+   *
+   * @param contentType a content type header
+   */
+  public static String getCharsetFromContentType(String contentType) {
+    if (contentType == null) {
+      return null;
+    }
+    int start = contentType.indexOf("charset=");
+    if (start < 0) {
+      return null;
+    }
+    String encoding = contentType.substring(start + 8);
+    int end = encoding.indexOf(';');
+    if (end >= 0) {
+      encoding = encoding.substring(0, end);
+    }
+    encoding = encoding.trim();
+    if ((encoding.length() > 2) && (encoding.startsWith("\""))
+        && (encoding.endsWith("\""))) {
+      encoding = encoding.substring(1, encoding.length() - 1);
+    }
+    return encoding.trim();
+  }
 }
