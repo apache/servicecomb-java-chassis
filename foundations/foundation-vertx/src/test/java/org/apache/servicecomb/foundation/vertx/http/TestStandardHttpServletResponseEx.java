ee04049d7024e10698ffc51b48dce84e2008d001
wujimin <wujimin@huawei.com>
2018-06-05 12:50:40 +0800

    [SCB-484] servlet response support send part

--- foundations/foundation-vertx/src/test/java/org/apache/servicecomb/foundation/vertx/http/TestStandardHttpServletResponseEx.java
+++ foundations/foundation-vertx/src/test/java/org/apache/servicecomb/foundation/vertx/http/TestStandardHttpServletResponseEx.java
@@ -17,12 +17,17 @@
 
 package org.apache.servicecomb.foundation.vertx.http;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.WriteListener;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.servicecomb.foundation.common.part.InputStreamPart;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,6 +36,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import io.vertx.core.buffer.Buffer;
+import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
 import mockit.Mocked;
@@ -132,9 +138,42 @@ public class TestStandardHttpServletResponseEx {
   }
 
   @Test
-  public void sendPart() {
-    setExceptionExpected();
+  public void sendPart_succ() throws Throwable {
+    String src = RandomStringUtils.random(100);
+    InputStream inputStream = new ByteArrayInputStream(src.getBytes());
+    Part part = new InputStreamPart("name", inputStream);
+    Buffer buffer = Buffer.buffer();
+    ServletOutputStream outputStream = new MockUp<ServletOutputStream>() {
+      @Mock
+      void write(int b) {
+        buffer.appendByte((byte) b);
+      }
+    }.getMockInstance();
 
-    responseEx.sendPart(null);
+    new Expectations() {
+      {
+        response.getOutputStream();
+        result = outputStream;
+      }
+    };
+
+    responseEx.sendPart(part).get();
+
+    Assert.assertEquals(src, buffer.toString());
+  }
+
+  @Test
+  public void sendPart_failed(@Mocked Part part) throws Throwable {
+    Error error = new Error();
+    new Expectations() {
+      {
+        response.getOutputStream();
+        result = error;
+      }
+    };
+
+    expectedException.expect(Matchers.sameInstance(error));
+
+    responseEx.sendPart(part).get();
   }
 }
