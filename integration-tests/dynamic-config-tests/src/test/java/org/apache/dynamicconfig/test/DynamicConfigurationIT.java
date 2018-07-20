c121ba897388f0625087ba6453c0d5c48f9dabc0
zhengyangyong <yangyong.zheng@huawei.com>
2018-07-20 14:52:15 +0800

    SCB-763 use vertx sim apollo server in order to simplify dynamic config (apollo) integration test
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- integration-tests/dynamic-config-tests/src/test/java/org/apache/dynamicconfig/test/DynamicConfigurationIT.java
+++ integration-tests/dynamic-config-tests/src/test/java/org/apache/dynamicconfig/test/DynamicConfigurationIT.java
@@ -17,112 +17,37 @@
 
 package org.apache.dynamicconfig.test;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.servicecomb.foundation.common.utils.BeanUtils;
 import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import com.netflix.config.DynamicPropertyFactory;
 
+import io.vertx.core.Vertx;
+
 public class DynamicConfigurationIT {
-  private static final Logger LOGGER = LoggerFactory.getLogger(DynamicConfigurationIT.class);
-
-  private static String url;
-
-  private static String token;
-
-  private static final HttpHeaders headers = new HttpHeaders();
-
-  private static final RestTemplate rest = new RestTemplate();
+  private static Vertx vertx = null;
 
   @BeforeClass
   public static void setUp() throws Exception {
-    LOGGER.info("testDynamicConfiguration set up");
+    vertx = Vertx.vertx();
+    vertx.deployVerticle(new SimApolloServer());
+
     Log4jUtils.init();
     BeanUtils.init();
-    url = DynamicPropertyFactory.getInstance().getStringProperty("apollo.config.serverUri", "missing").getValue();
-    token = DynamicPropertyFactory.getInstance().getStringProperty("apollo.config.token", "missing").getValue();
-    headers.add("Content-Type", "application/json;charset=UTF-8");
-    headers.add("Authorization", token);
   }
 
-  @After
-  public void tearDown() {
-    clearConfiguration();
+  @AfterClass
+  public static void tearDown() {
+    vertx.close();
   }
 
-  public int clearConfiguration() {
-    String delete = url
-        + "/openapi/v1/envs/DEV/apps/SampleApp/clusters/default/namespaces/application/items/loadbalance?operator=apollo";
-    HttpEntity<?> entity = new HttpEntity<Object>(headers);
-    ResponseEntity<String> exchange = rest.exchange(delete, HttpMethod.DELETE, entity, String.class);
-    Assert.assertEquals(exchange.getStatusCodeValue(), HttpStatus.OK);
-    return releaseConfiguration();
-  }
-
-  public int releaseConfiguration() {
-    String release = url + "/openapi/v1/envs/DEV/apps/SampleApp/clusters/default/namespaces/application/releases";
-    RestTemplate rest = new RestTemplate();
-    Map<String, String> body = new HashMap<>();
-    body.put("releaseTitle", "release-configuration");
-    body.put("releasedBy", "apollo");
-    HttpEntity<?> entity = new HttpEntity<Object>(body, headers);
-    ResponseEntity<String> exchange = rest.exchange(release, HttpMethod.POST, entity, String.class);
-    return exchange.getStatusCodeValue();
-  }
 
   @Test
   public void testDynamicConfiguration() {
-    LOGGER.info("testDynamicConfiguration starting");
-    //before
-    Assert.assertEquals(DynamicPropertyFactory.getInstance().getStringProperty("loadbalcance", "default").getValue(),
-        "default");
-
-    String setLoadBalance = url + "/openapi/v1/envs/DEV/apps/SampleApp/clusters/default/namespaces/application/items";
-    Map<String, String> body = new HashMap<>();
-    body.put("key", "loadbalance");
-    body.put("value", "roundrobbin");
-    body.put("dataChangeCreatedBy", "apollo");
-    HttpEntity<?> entity = new HttpEntity<Object>(body, headers);
-    ResponseEntity<String> exchange = rest.exchange(setLoadBalance, HttpMethod.POST, entity, String.class);
-    Assert.assertEquals(exchange.getStatusCodeValue(), HttpStatus.OK.value());
-    Assert.assertEquals(releaseConfiguration(), HttpStatus.OK);
-
-    await().atMost(5, SECONDS).until(
-        () -> DynamicPropertyFactory.getInstance().getStringProperty("loadbalance", "default").getValue()
-            .equals("roundrobbin"));
-
-    String updateLoadBalance =
-        url + "/openapi/v1/envs/DEV/apps/SampleApp/clusters/default/namespaces/application/items/" + "loadbalance";
-    body.clear();
-    body.put("key", "loadbalance");
-    body.put("value", "random");
-    body.put("dataChangeLastModifiedBy", "apollo");
-    entity = new HttpEntity<Object>(body, headers);
-    exchange = rest.exchange(updateLoadBalance, HttpMethod.PUT, entity, String.class);
-    Assert.assertEquals(exchange.getStatusCodeValue(), HttpStatus.OK.value());
-    Assert.assertEquals(releaseConfiguration(), HttpStatus.OK);
-
-    await().atMost(5, SECONDS).until(
-        () -> DynamicPropertyFactory.getInstance().getStringProperty("loadbalance", "default").getValue()
-            .equals("random"));
-
-    LOGGER.info("testDynamicConfiguration end");
+    Assert.assertEquals(6666, DynamicPropertyFactory.getInstance().getIntProperty("timeout", 0).get());
   }
 }
