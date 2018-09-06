564a0ce2814503be05868e8a93158347ff66d1a8
wujimin <wujimin@huawei.com>
2018-09-06 14:32:22 +0800

    [SCB-882] adjust TestParamCodec position

--- integration-tests/it-consumer/src/main/java/org/apache/servicecomb/it/testcase/TestParamCodec.java
+++ integration-tests/it-consumer/src/main/java/org/apache/servicecomb/it/testcase/TestParamCodec.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.servicecomb.it.testcase.base;
+package org.apache.servicecomb.it.testcase;
 
 import static org.junit.Assert.assertEquals;
 
