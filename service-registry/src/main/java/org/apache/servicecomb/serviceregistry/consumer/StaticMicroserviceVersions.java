92bf20b43da023d378f116bab0f66197b7177a3a
yaohaishi <yaohaishi@huawei.com>
2018-09-19 16:38:14 +0800

    [SCB-926] implement StaticMicroserviceVersions

--- service-registry/src/main/java/org/apache/servicecomb/serviceregistry/consumer/StaticMicroserviceVersions.java
+++ service-registry/src/main/java/org/apache/servicecomb/serviceregistry/consumer/StaticMicroserviceVersions.java
@@ -17,13 +17,20 @@
 
 package org.apache.servicecomb.serviceregistry.consumer;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import org.apache.servicecomb.serviceregistry.RegistryUtils;
 import org.apache.servicecomb.serviceregistry.api.registry.MicroserviceInstance;
+import org.apache.servicecomb.serviceregistry.api.registry.StaticMicroservice;
+import org.apache.servicecomb.serviceregistry.version.Version;
 
 public class StaticMicroserviceVersions extends MicroserviceVersions {
 
   private Class<?> schemaIntfCls;
+
+  private String environment;
 
   public StaticMicroserviceVersions(AppManager appManager, String appId, String microserviceName,
       Class<?> schemaIntfCls) {
@@ -31,6 +38,7 @@ public class StaticMicroserviceVersions extends MicroserviceVersions {
 
     validated = true;
     this.schemaIntfCls = schemaIntfCls;
+    this.environment = RegistryUtils.getMicroservice().getEnvironment();
   }
 
   @Override
@@ -38,7 +46,54 @@ public class StaticMicroserviceVersions extends MicroserviceVersions {
     // instance information is stored locally, do not pull from sc
   }
 
-  public void addInstances(String version, List<MicroserviceInstance> instances) {
+  public void addInstances(String version, List<MicroserviceInstance> addedInstances) {
+    Version parsedVersion = new Version(version);
+    String serviceId = computeServiceId(parsedVersion);
 
+    for (MicroserviceInstance instance : addedInstances) {
+      instance.setServiceId(serviceId);
+      instance.setInstanceId(serviceId + "-" + UUID.randomUUID());
+    }
+
+    mergeInstances(addedInstances);
+
+    // ensure microserviceVersion exists
+    versions.computeIfAbsent(serviceId, microserviceId -> {
+      StaticMicroservice microservice = createMicroservice(parsedVersion, serviceId);
+      MicroserviceVersion microserviceVersion = appManager.getStaticMicroserviceVersionFactory().create(microservice);
+      for (MicroserviceVersionRule microserviceVersionRule : versionRules.values()) {
+        microserviceVersionRule.addMicroserviceVersion(microserviceVersion);
+      }
+      return microserviceVersion;
+    });
+
+    for (MicroserviceVersionRule microserviceVersionRule : versionRules.values()) {
+      microserviceVersionRule.setInstances(this.instances);
+    }
+  }
+
+  private void mergeInstances(List<MicroserviceInstance> instances) {
+    if (null == this.instances) {
+      this.instances = new ArrayList<>(instances.size());
+    }
+    this.instances.addAll(instances);
+  }
+
+  private StaticMicroservice createMicroservice(Version parsedVersion, String serviceId) {
+    StaticMicroservice microservice = new StaticMicroservice();
+    microservice.setAppId(this.getAppId());
+    microservice.setServiceId(serviceId);
+    microservice.setServiceName(this.getMicroserviceName());
+    microservice.setVersion(parsedVersion.getVersion());
+    microservice.setEnvironment(RegistryUtils.getMicroservice().getEnvironment());
+    microservice.setSchemaIntfCls(this.schemaIntfCls);
+    return microservice;
+  }
+
+  private String computeServiceId(Version parsedVersion) {
+    return this.getAppId() + "-"
+        + this.environment + "-"
+        + this.getMicroserviceName() + "-"
+        + parsedVersion.getVersion();
   }
 }
