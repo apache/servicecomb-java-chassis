84310a6ad7b8516b356e268ce71118cd73ea0fb8
Yang Bo <yangbo12@huawei.com>
2018-11-24 11:14:00 +0800

    [SCB-1035] Fail CI on Rat error

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -20,50 +20,55 @@
 ##Check if the commit is tagged commit or not
 TAGGEDCOMMIT=$(git tag -l --contains HEAD)
 if [ "$TAGGEDCOMMIT" == "" ]; then
-        TAGGEDCOMMIT=false
+    TAGGEDCOMMIT=false
 else
-        TAGGEDCOMMIT=true
+    TAGGEDCOMMIT=true
 fi
 echo $TAGGEDCOMMIT
 
 
 if [ "$1" == "install" ]; then
-        if [ "$TAGGEDCOMMIT" == "true" ]; then
-              	echo "Skipping the installation as it is tagged commit"
-        else
-                mvn apache-rat:check -Pit,samples,distribution
-                mvn clean install -Ddocker.showLogs -Pdocker -Pjacoco -Pit -Pcoverage coveralls:report
-		if [ $? == 0 ]; then
-			echo "${green}Installation Success..${reset}"
-		else
-			echo "${red}Installation or Test Cases failed, please check the above logs for more details.${reset}"
-			exit 1
-		fi
+    if [ "$TAGGEDCOMMIT" == "true" ]; then
+        echo "Skipping the installation as it is tagged commit"
+    else
+        mvn apache-rat:check -Pit,samples,distribution
+        if [ $? != 0 ]; then
+            echo "${red}Rat check failed.${reset}"
+            exit 1
         fi
-        echo "Installation Completed"
+        
+        mvn clean install -Ddocker.showLogs -Pdocker -Pjacoco -Pit -Pcoverage coveralls:report
+	if [ $? == 0 ]; then
+	    echo "${green}Installation Success..${reset}"
+	else
+	    echo "${red}Installation or Test Cases failed, please check the above logs for more details.${reset}"
+	    exit 1
+	fi
+    fi
+    echo "Installation Completed"
 else
-        if [ "$TAGGEDCOMMIT" ==   "true" ]; then
-                echo "Decrypting the key"
-		openssl aes-256-cbc -K $encrypted_6d31958a1ad0_key -iv $encrypted_6d31958a1ad0_iv -in gpg-sec.tar.enc -out gpg-sec.tar -d
-		tar xvf gpg-sec.tar
-		echo "Deploying Staging Release"
-		mvn deploy -DskipTests -Prelease -Pdistribution -Ppassphrase --settings .travis.settings.xml
-		if [ $? == 0 ]; then
-			echo "${green}Staging Deployment is Success, please log on to Nexus Repo to see the staging release..${reset}"
-		else
-			echo "${red}Staging Release deployment failed.${reset}"
-			exit 1
-		fi
-        else
-		echo "Deploy a Non-Signed Staging Release"
-		mvn deploy -DskipTests --settings .travis.settings.xml
-		if [ $? == 0 ]; then
-			echo "${green}Snapshot Deployment is Success, please log on to Nexus Repo to see the snapshot release..${reset}"
-		else
-			echo "${red}Snapshot deployment failed.${reset}"
-			# No need to exit 1 here as the snapshot depoyment will fail for private builds as decryption of password is allowed for ServiceComb repo and not forked repo's.
-		fi
-                
-        fi
-	echo "Deployment Completed"
+    if [ "$TAGGEDCOMMIT" ==   "true" ]; then
+        echo "Decrypting the key"
+	openssl aes-256-cbc -K $encrypted_6d31958a1ad0_key -iv $encrypted_6d31958a1ad0_iv -in gpg-sec.tar.enc -out gpg-sec.tar -d
+	tar xvf gpg-sec.tar
+	echo "Deploying Staging Release"
+	mvn deploy -DskipTests -Prelease -Pdistribution -Ppassphrase --settings .travis.settings.xml
+	if [ $? == 0 ]; then
+	    echo "${green}Staging Deployment is Success, please log on to Nexus Repo to see the staging release..${reset}"
+	else
+	    echo "${red}Staging Release deployment failed.${reset}"
+	    exit 1
+	fi
+    else
+	echo "Deploy a Non-Signed Staging Release"
+	mvn deploy -DskipTests --settings .travis.settings.xml
+	if [ $? == 0 ]; then
+	    echo "${green}Snapshot Deployment is Success, please log on to Nexus Repo to see the snapshot release..${reset}"
+	else
+	    echo "${red}Snapshot deployment failed.${reset}"
+	    # No need to exit 1 here as the snapshot depoyment will fail for private builds as decryption of password is allowed for ServiceComb repo and not forked repo's.
+	fi
+        
+    fi
+    echo "Deployment Completed"
 fi 
