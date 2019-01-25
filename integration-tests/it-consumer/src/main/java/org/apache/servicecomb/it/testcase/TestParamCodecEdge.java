a13fa4ace4043c0a81746a60fe30cef6b30ca732
yaohaishi <yaohaishi@huawei.com>
2019-01-25 15:08:15 +0800

    [SCB-1129] add IT for special enum name

--- integration-tests/it-consumer/src/main/java/org/apache/servicecomb/it/testcase/TestParamCodecEdge.java
+++ integration-tests/it-consumer/src/main/java/org/apache/servicecomb/it/testcase/TestParamCodecEdge.java
@@ -19,6 +19,7 @@ package org.apache.servicecomb.it.testcase;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.servicecomb.foundation.test.scaffolding.model.Media;
 import org.apache.servicecomb.it.extend.engine.GateRestTemplate;
 import org.junit.Test;
 
@@ -30,5 +31,19 @@ public class TestParamCodecEdge {
     String paramString = "a%2B+%20b%% %20c";
     String result = client.getForObject("/spaceCharCodec/" + paramString + "?q=" + paramString, String.class);
     assertEquals(paramString + " +%20%% " + paramString + " true", result);
+  }
+
+  @Test
+  public void enumSpecialName() {
+    assertEquals(Media.AAC,
+        client.postForObject("/enum/enumSpecialName", Media.AAC, Media.class));
+    assertEquals(Media.FLAC,
+        client.postForObject("/enum/enumSpecialName", Media.FLAC, Media.class));
+    assertEquals(Media.H_264,
+        client.postForObject("/enum/enumSpecialName", Media.H_264, Media.class));
+    assertEquals(Media.MPEG_2,
+        client.postForObject("/enum/enumSpecialName", Media.MPEG_2, Media.class));
+    assertEquals(Media.WMV,
+        client.postForObject("/enum/enumSpecialName", Media.WMV, Media.class));
   }
 }
