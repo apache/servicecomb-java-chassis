dfa162449eb746af0b399627d1f834b86eaa2c3d
wujimin <wujimin@huawei.com>
2019-07-03 00:42:06 +0800

    [SCB-1344][WIP][WEAK] use producer parameter type in producer argument mapper for wrapped body

--- swagger/swagger-invocation/invocation-core/src/main/java/org/apache/servicecomb/swagger/invocation/arguments/producer/ProducerArgumentsMapperCreator.java
+++ swagger/swagger-invocation/invocation-core/src/main/java/org/apache/servicecomb/swagger/invocation/arguments/producer/ProducerArgumentsMapperCreator.java
@@ -21,6 +21,7 @@ import static org.apache.servicecomb.swagger.generator.SwaggerGeneratorUtils.col
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 import java.util.Map;
 
 import org.apache.servicecomb.foundation.common.utils.LambdaMetafactoryUtils;
@@ -36,15 +37,25 @@ import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
 public class ProducerArgumentsMapperCreator extends AbstractArgumentsMapperCreator {
+  // swagger parameter types relate to producer
+  // because features of @BeanParam/query, and rpc mode parameter wrapper
+  // types is not always equals to producerMethod parameter types directly
+  private Type[] swaggerParameterTypes;
+
   public ProducerArgumentsMapperCreator(SerializationConfig serializationConfig,
       Map<Class<?>, ContextArgumentMapperFactory> contextFactorys,
       Method producerMethod, SwaggerOperation swaggerOperation) {
     super(serializationConfig, contextFactorys, producerMethod, swaggerOperation);
+
+    swaggerParameterTypes = new Type[swaggerOperation.getOperation().getParameters().size()];
+  }
+
+  public Type[] getSwaggerParameterTypes() {
+    return swaggerParameterTypes;
   }
 
   public ProducerArgumentsMapper createArgumentsMapper() {
     doCreateArgumentsMapper();
-
     return new ProducerArgumentsMapper(mappers, providerMethod.getParameterCount());
   }
 
@@ -57,13 +68,16 @@ public class ProducerArgumentsMapperCreator extends AbstractArgumentsMapperCreat
 
   @Override
   protected ArgumentMapper createKnownParameterMapper(int producerParamIdx, Integer swaggerIdx) {
-    return new ProducerArgumentSame(producerParamIdx, swaggerIdx);
+    swaggerParameterTypes[swaggerIdx] = providerMethod.getGenericParameterTypes()[producerParamIdx];
+    return new ProducerArgumentSame(swaggerIdx, producerParamIdx);
   }
 
   @Override
   protected ArgumentMapper createSwaggerBodyFieldMapper(int producerParamIdx, String parameterName,
       int swaggerBodyIdx) {
-    return new SwaggerBodyFieldToProducerArgument(producerParamIdx, parameterName, swaggerBodyIdx);
+    swaggerParameterTypes[swaggerBodyIdx] = Object.class;
+    return new SwaggerBodyFieldToProducerArgument(producerParamIdx, parameterName,
+        providerMethod.getGenericParameterTypes()[producerParamIdx], swaggerBodyIdx);
   }
 
   @Override
@@ -87,6 +101,7 @@ public class ProducerArgumentsMapperCreator extends AbstractArgumentsMapperCreat
         setter = LambdaMetafactoryUtils.createSetter(propertyDefinition.getField().getAnnotated());
       }
 
+      swaggerParameterTypes[swaggerIdx] = propertyDefinition.getPrimaryType();
       mapper.addField(swaggerIdx, setter);
     }
     mappers.add(mapper);
