384c86b08a113cfa8a8bfb1f2f34fa9ec091b2e2
liubao <bismy@qq.com>
2020-03-30 15:39:03 +0800

    [SCB-1835] travis CI do not need release/deploy when cut release version

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -17,58 +17,25 @@
 ## ---------------------------------------------------------------------------
 #bin/sh
 
-##Check if the commit is tagged commit or not
-TAGGEDCOMMIT=$(git tag -l --contains HEAD)
-if [ "$TAGGEDCOMMIT" == "" ]; then
-    TAGGEDCOMMIT=false
-else
-    TAGGEDCOMMIT=true
-fi
-echo $TAGGEDCOMMIT
-
+## whenever commit to the repository, do Compilation and Installation
 
 if [ "$1" == "install" ]; then
-    if [ "$TAGGEDCOMMIT" == "true" ]; then
-        echo "Skipping the installation as it is tagged commit"
-    else
-        mvn apache-rat:check -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B -Pit,distribution
-        if [ $? != 0 ]; then
-            echo "${red}Rat check failed.${reset}"
-            exit 1
-        fi
-        
-        mvn clean install -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B -Pdocker -Pdemo-run-release -Pjacoco -Pit -Pcoverage coveralls:report
-	if [ $? == 0 ]; then
-	    echo "${green}Installation Success..${reset}"
-	else
-	    echo "${red}Installation or Test Cases failed, please check the above logs for more details.${reset}"
-	    exit 1
-	fi
+    mvn apache-rat:check -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B -Pit,distribution
+    if [ $? != 0 ]; then
+        echo "${red}Rat check failed.${reset}"
+        exit 1
     fi
-    echo "Installation Completed"
+
+    mvn clean install -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B -Pdocker -Pdemo-run-release -Pjacoco -Pit -Pcoverage coveralls:report
+    if [ $? == 0 ]; then
+        echo "${green}Installation Success..${reset}"
+    else
+        echo "${red}Installation or Test Cases failed, please check the above logs for more details.${reset}"
+        exit 1
+    fi
+
+    echo "Compilation and Installation Completed"
 else
-    if [ "$TAGGEDCOMMIT" ==   "true" ]; then
-        echo "Decrypting the key"
-	openssl aes-256-cbc -K $encrypted_6d31958a1ad0_key -iv $encrypted_6d31958a1ad0_iv -in gpg-sec.tar.enc -out gpg-sec.tar -d
-	tar xvf gpg-sec.tar
-	echo "Deploying Staging Release"
-	mvn deploy -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B -DskipTests -Prelease -Pdistribution -Ppassphrase --settings .travis.settings.xml
-	if [ $? == 0 ]; then
-	    echo "${green}Staging Deployment is Success, please log on to Nexus Repo to see the staging release..${reset}"
-	else
-	    echo "${red}Staging Release deployment failed.${reset}"
-	    exit 1
-	fi
-    else
-	echo "Deploy a Non-Signed Staging Release"
-	mvn deploy -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B -DskipTests --settings .travis.settings.xml
-	if [ $? == 0 ]; then
-	    echo "${green}Snapshot Deployment is Success, please log on to Nexus Repo to see the snapshot release..${reset}"
-	else
-	    echo "${red}Snapshot deployment failed.${reset}"
-	    # No need to exit 1 here as the snapshot depoyment will fail for private builds as decryption of password is allowed for ServiceComb repo and not forked repo's.
-	fi
-        
-    fi
-    echo "Deployment Completed"
+    echo "Not Implemented parameter"
+    exit 1
 fi 
