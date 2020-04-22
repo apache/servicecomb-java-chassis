430a5e2a972c35e5f647f358adb5d11dd31d1b97
liubao <bismy@qq.com>
2020-04-22 10:00:35 +0800

    [SCB-1873] using Version to implement VrsionCompareUtil

--- handlers/handler-router/src/main/java/org/apache/servicecomb/router/util/VersionCompareUtil.java
+++ handlers/handler-router/src/main/java/org/apache/servicecomb/router/util/VersionCompareUtil.java
@@ -16,39 +16,10 @@
  */
 package org.apache.servicecomb.router.util;
 
-/**
- * @Author GuoYl123
- * @Date 2019/10/16
- **/
+import org.apache.servicecomb.serviceregistry.version.Version;
+
 public class VersionCompareUtil {
-    /**
-     * if first num is bigger then return a positive number
-     *
-     * @param version1
-     * @param version2
-     * @return
-     */
-    public static int compareVersion(String version1, String version2) {
-        if (version1 == null || version2 == null) {
-            throw new RuntimeException("version can not be null");
-        }
-        String[] versionArray1 = version1.split("\\.");
-        String[] versionArray2 = version2.split("\\.");
-        int idx = 0;
-        int diff = 0;
-        int minLength = Math.min(versionArray1.length, versionArray2.length);
-        while (idx < minLength
-                && versionArray1[idx].length() - versionArray2[idx].length() == 0
-                && versionArray1[idx].compareTo(versionArray2[idx]) == 0) {
-            ++idx;
-        }
-        idx = idx < minLength ? idx : idx - 1;
-        if (versionArray1[idx].length() - versionArray2[idx].length() == 0) {
-            diff = versionArray1[idx].compareTo(versionArray2[idx]);
-        } else {
-            diff = versionArray1[idx].length() - versionArray2[idx].length();
-        }
-        diff = (diff != 0) ? diff : versionArray1.length - versionArray2.length;
-        return diff;
-    }
+  public static int compareVersion(String version1, String version2) {
+    return new Version(version1).compareTo(new Version(version2));
+  }
 }
