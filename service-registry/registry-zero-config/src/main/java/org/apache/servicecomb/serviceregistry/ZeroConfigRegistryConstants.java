18fa8a7ea095f3af0d3efaf9b6b5db095728d49a
Jun Gan <jungan21@gmail.com>
2020-05-17 21:25:57 -0400

    add sub-module for registry-zero-config

--- service-registry/registry-zero-config/src/main/java/org/apache/servicecomb/serviceregistry/ZeroConfigRegistryConstants.java
+++ service-registry/registry-zero-config/src/main/java/org/apache/servicecomb/serviceregistry/ZeroConfigRegistryConstants.java
@@ -1,6 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicecomb.serviceregistry;
 
-public interface ZeroConfigRegistryConstants {
+public class ZeroConfigRegistryConstants {
 
     String ZERO_CONFIG_REGISTRY_FLAG = "zeroconfig.registry.flag";
     String ZERO_CONFIG_REGISTRY_ENABLE_FLAG = "enable";
@@ -11,7 +27,7 @@ public interface ZeroConfigRegistryConstants {
     /**
      * ttl=1，local network, ttl=32，local network station, ttl=64，local area；ttl=128，local continent
      * ttl=255，all places. Default value is 1;
-      */
+     */
     Integer TIME_TO_LIVE = 1;
     Integer DATA_PACKET_BUFFER_SIZE = 2048; // 2K
     long HEALTH_CHECK_INTERVAL = 3;
@@ -52,4 +68,5 @@ public interface ZeroConfigRegistryConstants {
     String SERVICE_ID_SPLITER = "/";
     String ENDPOINT_PREFIX_REST = "rest";
     String ENDPOINT_PREFIX_HTTP = "http";
+
 }
--- service-registry/registry-zero-config/src/main/java/org/apache/servicecomb/serviceregistry/server/ServerMicroserviceInstance.java
+++ service-registry/registry-zero-config/src/main/java/org/apache/servicecomb/serviceregistry/server/ServerMicroserviceInstance.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicecomb.serviceregistry.server;
 
 import java.time.Instant;
@@ -5,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServerMicroserviceInstance {
-
     private String appId;
 
     private String serviceName;
@@ -123,5 +138,4 @@ public class ServerMicroserviceInstance {
                 ", lastHeartbeatTimeStamp=" + lastHeartbeatTimeStamp +
                 '}';
     }
-
 }
