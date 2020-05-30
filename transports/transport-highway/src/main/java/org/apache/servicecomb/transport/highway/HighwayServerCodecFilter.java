942e57bfad4b49a6df595d136a3a5b15755dee2a
wujimin <wujimin@huawei.com>
2020-05-30 17:29:26 +0800

    [SCB-1962] add highway server codec filter

---
+++ transports/transport-highway/src/main/java/org/apache/servicecomb/transport/highway/HighwayServerCodecFilter.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicecomb.transport.highway;
+
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static org.apache.servicecomb.core.exception.Exceptions.exceptionToResponse;
+import static org.apache.servicecomb.swagger.invocation.InvocationType.PRODUCER;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.servicecomb.codec.protobuf.definition.OperationProtobuf;
+import org.apache.servicecomb.codec.protobuf.definition.ResponseRootSerializer;
+import org.apache.servicecomb.core.Invocation;
+import org.apache.servicecomb.core.filter.Filter;
+import org.apache.servicecomb.core.filter.FilterMeta;
+import org.apache.servicecomb.core.filter.FilterNode;
+import org.apache.servicecomb.foundation.common.utils.AsyncUtils;
+import org.apache.servicecomb.swagger.invocation.Response;
+import org.apache.servicecomb.transport.highway.message.ResponseHeader;
+
+import io.vertx.core.buffer.Buffer;
+
+@FilterMeta(name = "highway-server-codec", invocationType = PRODUCER)
+public class HighwayServerCodecFilter implements Filter {
+  @Override
+  public CompletableFuture<Response> onFilter(Invocation invocation, FilterNode nextNode) {
+    return CompletableFuture.completedFuture(invocation)
+        .thenCompose(this::decodeRequest)
+        .thenCompose(nextNode::onFilter)
+        .exceptionally(exception -> exceptionToResponse(invocation, exception, INTERNAL_SERVER_ERROR))
+        .thenCompose(response -> encodeResponse(invocation, response));
+  }
+
+  private CompletableFuture<Invocation> decodeRequest(Invocation invocation) {
+    HighwayTransportContext transportContext = invocation.getTransportContext();
+    try {
+      HighwayCodec.decodeRequest(invocation,
+          transportContext.getHeader(),
+          transportContext.getOperationProtobuf(),
+          transportContext.getBodyBuffer());
+      return CompletableFuture.completedFuture(invocation);
+    } catch (Exception e) {
+      return AsyncUtils.completeExceptionally(e);
+    }
+  }
+
+  private CompletableFuture<Response> encodeResponse(Invocation invocation, Response response) {
+    ResponseHeader header = new ResponseHeader();
+    header.setStatusCode(response.getStatusCode());
+    header.setReasonPhrase(response.getReasonPhrase());
+    header.setContext(invocation.getContext());
+    header.setHeaders(response.getHeaders());
+
+    HighwayTransportContext transportContext = invocation.getTransportContext();
+    long msgId = transportContext.getMsgId();
+    OperationProtobuf operationProtobuf = transportContext.getOperationProtobuf();
+    ResponseRootSerializer bodySchema = operationProtobuf.findResponseRootSerializer(response.getStatusCode());
+
+    try {
+      Buffer respBuffer = HighwayCodec.encodeResponse(
+          msgId, header, bodySchema, response.getResult());
+      transportContext.setResponseBuffer(respBuffer);
+
+      return CompletableFuture.completedFuture(response);
+    } catch (Exception e) {
+      // keep highway performance and simple, this encoding/decoding error not need handle by client
+      String msg = String.format("encode response failed, msgId=%d", msgId);
+      return AsyncUtils.completeExceptionally(new IllegalStateException(msg, e));
+    }
+  }
+}
