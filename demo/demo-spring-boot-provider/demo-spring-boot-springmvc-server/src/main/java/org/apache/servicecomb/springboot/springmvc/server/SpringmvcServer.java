e622d17dd58866a5ea7d5800175c27ac6880fbcb
liubao <bismy@qq.com>
2020-06-23 19:33:17 +0800

    [SCB-2024]support not resolve place holder when running in spring boot

--- demo/demo-spring-boot-provider/demo-spring-boot-springmvc-server/src/main/java/org/apache/servicecomb/springboot/springmvc/server/SpringmvcServer.java
+++ demo/demo-spring-boot-provider/demo-spring-boot-springmvc-server/src/main/java/org/apache/servicecomb/springboot/springmvc/server/SpringmvcServer.java
@@ -17,16 +17,33 @@
 
 package org.apache.servicecomb.springboot.springmvc.server;
 
+import org.apache.servicecomb.core.SCBEngine;
 import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import com.netflix.config.DynamicPropertyFactory;
+
 @SpringBootApplication
 @EnableServiceComb
 public class SpringmvcServer {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SpringmvcServer.class);
 
   public static void main(final String[] args) throws Exception {
     new SpringApplicationBuilder().sources(SpringmvcServer.class).web(WebApplicationType.NONE).build().run(args);
+
+    assertPropertyCorrect();
+  }
+
+  private static void assertPropertyCorrect() {
+    String result = DynamicPropertyFactory.getInstance()
+        .getStringProperty("test.unresolved.placeholder", null).get();
+    if (!"jdbc:postgresql://${ip}:${port}/pt".equals(result)) {
+      LOGGER.error("tests for configuration error, stop");
+      SCBEngine.getInstance().destroy();
+    }
   }
 }
