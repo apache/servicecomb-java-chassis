6fbe38345dfb742d605c1ac6791276609b00a34a
ZhangJian He <shoothzj@gmail.com>
2022-05-13 22:20:20 +0800

    [SCB-2490] Bump mockito version from 3.3.3 to 4.5.1

--- handlers/handler-fault-injection/src/test/java/org/apache/servicecomb/faultinjection/TestFaultInjectHandler.java
+++ handlers/handler-fault-injection/src/test/java/org/apache/servicecomb/faultinjection/TestFaultInjectHandler.java
@@ -84,7 +84,7 @@ public class TestFaultInjectHandler {
     asyncResp = Mockito.mock(AsyncResponse.class);
     operationMeta = Mockito.mock(OperationMeta.class);
     transport = Mockito.mock(Transport.class);
-    MockitoAnnotations.initMocks(this);
+    MockitoAnnotations.openMocks(this);
   }
 
   @After
