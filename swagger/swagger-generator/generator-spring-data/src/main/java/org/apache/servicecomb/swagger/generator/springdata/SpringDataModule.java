544cac1a5265d60db1c13b215777843ee0e1795d
lovehzj <1922919664@qq.com>
2022-06-22 21:40:10 +0800

    fix checkstyle

--- swagger/swagger-generator/generator-spring-data/src/main/java/org/apache/servicecomb/swagger/generator/springdata/SpringDataModule.java
+++ swagger/swagger-generator/generator-spring-data/src/main/java/org/apache/servicecomb/swagger/generator/springdata/SpringDataModule.java
@@ -17,7 +17,6 @@
 package org.apache.servicecomb.swagger.generator.springdata;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.servicecomb.foundation.common.utils.SPIOrder;
