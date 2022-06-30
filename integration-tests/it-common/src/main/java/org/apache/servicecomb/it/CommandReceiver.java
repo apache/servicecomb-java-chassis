91b965e8b62ce051298e3d276bbd8b64fabda591
lovehzj <1922919664@qq.com>
2022-06-30 18:59:13 +0800

    [SCB-2626] make spotbugs check integrations

--- integration-tests/it-common/src/main/java/org/apache/servicecomb/it/CommandReceiver.java
+++ integration-tests/it-common/src/main/java/org/apache/servicecomb/it/CommandReceiver.java
@@ -19,7 +19,6 @@ package org.apache.servicecomb.it;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
