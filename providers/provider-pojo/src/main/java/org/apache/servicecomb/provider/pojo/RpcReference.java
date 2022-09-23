b7dccdbfbedf055f895a1573ef9e174f874de7bd
liubao <bismy@qq.com>
2022-09-23 15:45:48 +0800

    [SCB-2424]RpcReference support setter injection

--- providers/provider-pojo/src/main/java/org/apache/servicecomb/provider/pojo/RpcReference.java
+++ providers/provider-pojo/src/main/java/org/apache/servicecomb/provider/pojo/RpcReference.java
@@ -19,6 +19,7 @@ package org.apache.servicecomb.provider.pojo;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -27,7 +28,7 @@ import java.lang.annotation.Target;
 
 @Documented
 @Retention(RUNTIME)
-@Target({FIELD, ANNOTATION_TYPE})
+@Target({FIELD, METHOD, ANNOTATION_TYPE})
 public @interface RpcReference {
   String microserviceName();
 
