90437ee076ed513493c3e932840891757cf99163
liubao68 <bismy@qq.com>
2022-10-21 18:37:37 +0800

    add a test case for upload using String with Spring MVC (#3419)

--- demo/demo-springmvc/springmvc-server/src/main/java/org/apache/servicecomb/demo/springmvc/server/UploadSchema.java
+++ demo/demo-springmvc/springmvc-server/src/main/java/org/apache/servicecomb/demo/springmvc/server/UploadSchema.java
@@ -19,15 +19,19 @@ package org.apache.servicecomb.demo.springmvc.server;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.servicecomb.demo.TestMgr;
 import org.apache.servicecomb.provider.rest.common.RestSchema;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -63,7 +67,22 @@ public class UploadSchema {
   @PostMapping(path = "/fileUploadMultiRpc", produces = MediaType.TEXT_PLAIN_VALUE,
       consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public String fileUploadMultiRpc(@RequestPart(name = "files") MultipartFile[] files) {
-      return "fileUploadMulti success, and fileNum is " + files.length;
+    return "fileUploadMulti success, and fileNum is " + files.length;
   }
 
+  @PostMapping(path = "/uploadFileRequestPartAttribute", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String uploadFileRequestPartAttribute(@RequestPart(name = "file") MultipartFile file,
+      @RequestPart(name = "attribute") String attribute) throws IOException {
+    try (InputStream is = file.getInputStream()) {
+      return attribute + " " + IOUtils.toString(is, StandardCharsets.UTF_8);
+    }
+  }
+
+  @PostMapping(path = "/uploadFileAndAttribute", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String uploadFileAndAttribute(@RequestPart(name = "file") MultipartFile file,
+      @RequestAttribute(name = "attribute") String attribute) throws IOException {
+    try (InputStream is = file.getInputStream()) {
+      return attribute + " " + IOUtils.toString(is, StandardCharsets.UTF_8);
+    }
+  }
 }
