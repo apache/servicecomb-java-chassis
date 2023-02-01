2f3b86688d4f87810b8f5757cf4caecba34b65f2
liubao <bismy@qq.com>
2023-02-01 20:40:41 +0800

    [SCB-2757]remove spring initializer support and use spring boot to start java chassis

--- demo/demo-multiple/multiple-client/src/test/java/org/apache/servicecomb/demo/multiple/client/MultipleIT.java
+++ demo/demo-multiple/multiple-client/src/test/java/org/apache/servicecomb/demo/multiple/client/MultipleIT.java
@@ -14,31 +14,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.servicecomb.demo.multiple.client;
 
 import org.apache.servicecomb.demo.TestMgr;
-import org.apache.servicecomb.demo.multiple.a.client.AClient;
-import org.apache.servicecomb.demo.multiple.b.client.BClient;
-import org.apache.servicecomb.foundation.common.utils.BeanUtils;
-import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@EnableServiceComb
-public class MultipleClient {
-  public static void main(String[] args) throws Exception {
-    new SpringApplicationBuilder(MultipleClient.class).web(WebApplicationType.NONE).run(args);
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = MultipleClient.class)
+public class MultipleIT {
 
-    runTest();
+  @BeforeEach
+  public void setUp() {
+    TestMgr.errors().clear();
   }
 
-  public static void runTest() {
-    AClient aClient = BeanUtils.getContext().getBean(AClient.class);
-    BClient bClient = BeanUtils.getContext().getBean(BClient.class);
+  @Test
+  public void clientGetsNoError() throws Exception {
+    MultipleClient.runTest();
 
-    aClient.run();
-    bClient.run();
-
-    TestMgr.summary();
+    Assertions.assertTrue(TestMgr.errors().isEmpty());
   }
 }
