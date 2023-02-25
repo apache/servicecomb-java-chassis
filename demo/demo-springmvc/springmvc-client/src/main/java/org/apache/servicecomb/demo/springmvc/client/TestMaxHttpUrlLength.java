0634ce5815016740b3ca79556e711e38cd40e815
ZhangJian He <shoothzj@gmail.com>
2023-02-25 18:00:42 +0800

    [SCB-2781] Bump netty.version from 4.1.87.Final to 4.1.89.Final (#3659)

--- demo/demo-springmvc/springmvc-client/src/main/java/org/apache/servicecomb/demo/springmvc/client/TestMaxHttpUrlLength.java
+++ demo/demo-springmvc/springmvc-client/src/main/java/org/apache/servicecomb/demo/springmvc/client/TestMaxHttpUrlLength.java
@@ -38,13 +38,13 @@ public class TestMaxHttpUrlLength implements CategorizedTestCase {
 
   private void testUrlNotLongerThan4096() {
     RestTemplate restTemplate = RestTemplateBuilder.create();
-
-    String q = Strings.repeat("q", 4096 - "GET /api/springmvc/controller/sayhi?name=".length() - " HTTP/1.1\r".length());
+    // \r doesn't count for url length Since netty 4.1.88.Final See https://github.com/netty/netty/pull/12321
+    String q = Strings.repeat("q", 4096 + 1 - "GET /api/springmvc/controller/sayhi?name=".length() - " HTTP/1.1\r".length());
     TestMgr.check("hi " + q + " [" + q + "]",
         restTemplate.getForObject("cse://springmvc/springmvc/controller/sayhi?name=" + q,
             String.class));
 
-    q = Strings.repeat("q", 4096 + 1 - "GET /api/springmvc/controller/sayhi?name=".length() - " HTTP/1.1\r".length());
+    q = Strings.repeat("q", 4096 + 2 - "GET /api/springmvc/controller/sayhi?name=".length() - " HTTP/1.1\r".length());
     try {
       restTemplate.getForObject("cse://springmvc/springmvc/controller/sayhi?name=" + q,
           String.class);
