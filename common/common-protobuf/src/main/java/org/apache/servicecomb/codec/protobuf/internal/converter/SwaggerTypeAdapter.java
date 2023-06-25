e081c94ef7d6b7f9aa1b4def614bfdabb9bfdbfc
liubao <bismy@qq.com>
2023-06-25 09:58:32 +0800

    [SCB-2687]upgrade to open api 3: proto highway

--- common/common-protobuf/src/main/java/org/apache/servicecomb/codec/protobuf/internal/converter/SwaggerTypeAdapter.java
+++ common/common-protobuf/src/main/java/org/apache/servicecomb/codec/protobuf/internal/converter/SwaggerTypeAdapter.java
@@ -18,31 +18,12 @@ package org.apache.servicecomb.codec.protobuf.internal.converter;
 
 import java.util.List;
 
-import io.swagger.models.Model;
-import io.swagger.models.parameters.BodyParameter;
-import io.swagger.models.parameters.SerializableParameter;
-import io.swagger.models.properties.Property;
+import io.swagger.v3.oas.models.media.Schema;
 
 public interface SwaggerTypeAdapter {
-  static SwaggerTypeAdapter create(Object swaggerType) {
+  static SwaggerTypeAdapter create(Schema swaggerType) {
     if (swaggerType instanceof SwaggerTypeAdapter) {
       return (SwaggerTypeAdapter) swaggerType;
-    }
-
-    if (swaggerType instanceof Model) {
-      return new ModelAdapter((Model) swaggerType);
-    }
-
-    if (swaggerType instanceof Property) {
-      return new PropertyAdapter((Property) swaggerType);
-    }
-
-    if (swaggerType instanceof SerializableParameter) {
-      return new SerializableParameterAdapter((SerializableParameter) swaggerType);
-    }
-
-    if (swaggerType instanceof BodyParameter) {
-      return new BodyParameterAdapter((BodyParameter) swaggerType);
     }
 
     throw new IllegalStateException("not support swagger type: " + swaggerType.getClass().getName());
@@ -50,9 +31,9 @@ public interface SwaggerTypeAdapter {
 
   String getRefType();
 
-  Property getArrayItem();
+  Schema getArrayItem();
 
-  Property getMapItem();
+  Schema getMapItem();
 
   List<String> getEnum();
 
