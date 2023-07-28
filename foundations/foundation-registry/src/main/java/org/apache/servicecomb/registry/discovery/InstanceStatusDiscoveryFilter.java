8e67e4ed82f0f46788c9f44fc9d8a4fcb1a7a1b0
liubao <bismy@qq.com>
2023-07-28 15:29:54 +0800

    [SCB-2799]add instance group core logic

--- foundations/foundation-registry/src/main/java/org/apache/servicecomb/registry/discovery/InstanceStatusDiscoveryFilter.java
+++ foundations/foundation-registry/src/main/java/org/apache/servicecomb/registry/discovery/InstanceStatusDiscoveryFilter.java
@@ -21,12 +21,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.servicecomb.registry.api.MicroserviceInstanceStatus;
+import org.apache.servicecomb.registry.discovery.StatefulDiscoveryInstance.HistoryStatus;
+import org.apache.servicecomb.registry.discovery.StatefulDiscoveryInstance.IsolationStatus;
+import org.apache.servicecomb.registry.discovery.StatefulDiscoveryInstance.PingStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
-import com.netflix.config.DynamicPropertyFactory;
-
-// TODO: this class should be removed and add new filter to address state group
 public class InstanceStatusDiscoveryFilter extends AbstractDiscoveryFilter {
-  private static final String UP_INSTANCES = "upInstances";
+  public static final String PARAMETER = "status_level";
+
+  public static final String INSTANCE_GROUP_KEY_PREFIX = "status_group_";
+
+  public static final int MAX_GROUP = 3;
+
+  private Environment environment;
+
+  @Autowired
+  public void setEnvironment(Environment environment) {
+    this.environment = environment;
+  }
 
   @Override
   public int getOrder() {
@@ -35,8 +48,7 @@ public class InstanceStatusDiscoveryFilter extends AbstractDiscoveryFilter {
 
   @Override
   public boolean enabled() {
-    return DynamicPropertyFactory.getInstance()
-        .getBooleanProperty("servicecomb.loadbalance.filter.status.enabled", true).get();
+    return environment.getProperty("servicecomb.loadbalance.filter.status.enabled", Boolean.class, true);
   }
 
   @Override
@@ -46,22 +58,64 @@ public class InstanceStatusDiscoveryFilter extends AbstractDiscoveryFilter {
 
   @Override
   protected String findChildName(DiscoveryContext context, DiscoveryTreeNode parent) {
-    return UP_INSTANCES;
+    Integer level = context.getContextParameter(PARAMETER);
+    String group;
+    if (level == null) {
+      group = INSTANCE_GROUP_KEY_PREFIX + 0;
+      context.pushRerunFilter();
+      context.putContextParameter(PARAMETER, 0);
+      return group;
+    }
+
+    level = level + 1;
+    group = INSTANCE_GROUP_KEY_PREFIX + level;
+    context.putContextParameter(PARAMETER, level);
+
+    if (level < MAX_GROUP) {
+      context.pushRerunFilter();
+    }
+    return group;
   }
 
   @Override
   public void init(DiscoveryContext context, DiscoveryTreeNode parent) {
     List<StatefulDiscoveryInstance> instances = parent.data();
-    List<StatefulDiscoveryInstance> filteredServers = new ArrayList<>();
+    List<StatefulDiscoveryInstance> level0 = new ArrayList<>();
+    List<StatefulDiscoveryInstance> level1 = new ArrayList<>();
+    List<StatefulDiscoveryInstance> level2 = new ArrayList<>();
+    List<StatefulDiscoveryInstance> level3 = new ArrayList<>();
     for (StatefulDiscoveryInstance instance : instances) {
-      if (MicroserviceInstanceStatus.UP == instance.getMicroserviceInstanceStatus()) {
-        filteredServers.add(instance);
+      if (HistoryStatus.CURRENT == instance.getHistoryStatus() &&
+          MicroserviceInstanceStatus.UP == instance.getMicroserviceInstanceStatus() &&
+          PingStatus.OK == instance.getPingStatus() &&
+          IsolationStatus.NORMAL == instance.getIsolationStatus()) {
+        level0.add(instance);
+        continue;
       }
+      if (HistoryStatus.CURRENT == instance.getHistoryStatus() &&
+          MicroserviceInstanceStatus.UP == instance.getMicroserviceInstanceStatus() &&
+          PingStatus.UNKNOWN == instance.getPingStatus() &&
+          IsolationStatus.NORMAL == instance.getIsolationStatus()) {
+        level1.add(instance);
+        continue;
+      }
+      if (HistoryStatus.HISTORY == instance.getHistoryStatus() &&
+          MicroserviceInstanceStatus.UP == instance.getMicroserviceInstanceStatus() &&
+          PingStatus.OK == instance.getPingStatus() &&
+          IsolationStatus.NORMAL == instance.getIsolationStatus()) {
+        level2.add(instance);
+        continue;
+      }
+      level3.add(instance);
     }
-    if (filteredServers.isEmpty()) {
-      return;
-    }
-    DiscoveryTreeNode child = new DiscoveryTreeNode().subName(parent, UP_INSTANCES).data(filteredServers);
-    parent.child(UP_INSTANCES, child);
+
+    parent.child(INSTANCE_GROUP_KEY_PREFIX + 0, new DiscoveryTreeNode()
+        .subName(parent, INSTANCE_GROUP_KEY_PREFIX + 0).data(level0));
+    parent.child(INSTANCE_GROUP_KEY_PREFIX + 1, new DiscoveryTreeNode()
+        .subName(parent, INSTANCE_GROUP_KEY_PREFIX + 1).data(level1));
+    parent.child(INSTANCE_GROUP_KEY_PREFIX + 2, new DiscoveryTreeNode()
+        .subName(parent, INSTANCE_GROUP_KEY_PREFIX + 2).data(level2));
+    parent.child(INSTANCE_GROUP_KEY_PREFIX + 3, new DiscoveryTreeNode()
+        .subName(parent, INSTANCE_GROUP_KEY_PREFIX + 3).data(level3));
   }
 }
---
+++ foundations/foundation-registry/src/test/java/org/apache/servicecomb/registry/discovery/TestInstanceStatusDiscoveryFilter.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicecomb.registry.discovery;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.servicecomb.registry.api.MicroserviceInstanceStatus;
+import org.apache.servicecomb.registry.discovery.StatefulDiscoveryInstance.HistoryStatus;
+import org.apache.servicecomb.registry.discovery.StatefulDiscoveryInstance.IsolationStatus;
+import org.apache.servicecomb.registry.discovery.StatefulDiscoveryInstance.PingStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class TestInstanceStatusDiscoveryFilter {
+  @Test
+  public void test_all_group_correct_init() {
+    InstanceStatusDiscoveryFilter filter = new InstanceStatusDiscoveryFilter();
+    DiscoveryTreeNode parent = new DiscoveryTreeNode();
+    List<StatefulDiscoveryInstance> instances = new ArrayList<>();
+    StatefulDiscoveryInstance instance1 = Mockito.mock(StatefulDiscoveryInstance.class);
+    Mockito.when(instance1.getHistoryStatus()).thenReturn(HistoryStatus.CURRENT);
+    Mockito.when(instance1.getMicroserviceInstanceStatus()).thenReturn(MicroserviceInstanceStatus.UP);
+    Mockito.when(instance1.getPingStatus()).thenReturn(PingStatus.OK);
+    Mockito.when(instance1.getIsolationStatus()).thenReturn(IsolationStatus.NORMAL);
+    StatefulDiscoveryInstance instance2 = Mockito.mock(StatefulDiscoveryInstance.class);
+    Mockito.when(instance2.getHistoryStatus()).thenReturn(HistoryStatus.CURRENT);
+    Mockito.when(instance2.getMicroserviceInstanceStatus()).thenReturn(MicroserviceInstanceStatus.UP);
+    Mockito.when(instance2.getPingStatus()).thenReturn(PingStatus.UNKNOWN);
+    Mockito.when(instance2.getIsolationStatus()).thenReturn(IsolationStatus.NORMAL);
+    StatefulDiscoveryInstance instance3 = Mockito.mock(StatefulDiscoveryInstance.class);
+    Mockito.when(instance3.getHistoryStatus()).thenReturn(HistoryStatus.HISTORY);
+    Mockito.when(instance3.getMicroserviceInstanceStatus()).thenReturn(MicroserviceInstanceStatus.UP);
+    Mockito.when(instance3.getPingStatus()).thenReturn(PingStatus.OK);
+    Mockito.when(instance3.getIsolationStatus()).thenReturn(IsolationStatus.NORMAL);
+    StatefulDiscoveryInstance instance4 = Mockito.mock(StatefulDiscoveryInstance.class);
+    Mockito.when(instance4.getHistoryStatus()).thenReturn(HistoryStatus.CURRENT);
+    Mockito.when(instance4.getMicroserviceInstanceStatus()).thenReturn(MicroserviceInstanceStatus.UP);
+    Mockito.when(instance4.getPingStatus()).thenReturn(PingStatus.FAIL);
+    Mockito.when(instance4.getIsolationStatus()).thenReturn(IsolationStatus.NORMAL);
+    instances.addAll(Arrays.asList(instance1, instance2, instance3, instance4));
+
+    parent.name("parent");
+    parent.data(instances);
+    filter.init(new DiscoveryContext(), parent);
+
+    List<StatefulDiscoveryInstance> level0 = parent
+        .child(InstanceStatusDiscoveryFilter.INSTANCE_GROUP_KEY_PREFIX + 0)
+        .data();
+    Assertions.assertEquals(1, level0.size());
+    List<StatefulDiscoveryInstance> level1 = parent
+        .child(InstanceStatusDiscoveryFilter.INSTANCE_GROUP_KEY_PREFIX + 1)
+        .data();
+    Assertions.assertEquals(1, level1.size());
+    Assertions.assertEquals(1, level1.size());
+    List<StatefulDiscoveryInstance> level2 = parent
+        .child(InstanceStatusDiscoveryFilter.INSTANCE_GROUP_KEY_PREFIX + 2)
+        .data();
+    Assertions.assertEquals(1, level2.size());
+    List<StatefulDiscoveryInstance> level3 = parent
+        .child(InstanceStatusDiscoveryFilter.INSTANCE_GROUP_KEY_PREFIX + 3)
+        .data();
+    Assertions.assertEquals(1, level3.size());
+
+    Assertions.assertEquals(null, parent
+        .child(InstanceStatusDiscoveryFilter.INSTANCE_GROUP_KEY_PREFIX + 4));
+  }
+}
