4950f448d885eab79eca543503bc6f711db9d32e
chengyouling <spadgerlin888@163.com>
2023-11-08 17:37:41 +0800

    update KieAddressManager firstPull address choose

--- clients/config-kie-client/src/main/java/org/apache/servicecomb/config/kie/client/KieConfigManager.java
+++ clients/config-kie-client/src/main/java/org/apache/servicecomb/config/kie/client/KieConfigManager.java
@@ -68,10 +68,10 @@ public class KieConfigManager extends AbstractTask {
   public void firstPull() {
     try {
       Map<String, Object> data = new HashMap<>();
+      String address = kieAddressManager.chooseFirstPullAddress();
       this.configurationsRequests.forEach(r -> {
         r.setRevision(ConfigurationsRequest.INITIAL_REVISION);
-        ConfigurationsResponse response = configKieClient.queryConfigurations(r,
-            kieAddressManager.chooseFirstPullAddress());
+        ConfigurationsResponse response = configKieClient.queryConfigurations(r,address);
         if (response.isChanged()) {
           r.setRevision(response.getRevision());
           r.setLastRawData(response.getConfigurations());
