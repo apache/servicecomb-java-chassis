499c8ee61de25d41090d93b58d6f9abfde66615c
liubao <bismy@qq.com>
2024-04-01 11:13:10 +0800

    [SCB-2863]support zookeeper registry

---
+++ service-registry/registry-zookeeper/src/main/java/org/apache/servicecomb/registry/zookeeper/ZookeeperRegistryProperties.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicecomb.registry.zookeeper;
+
+public class ZookeeperRegistryProperties {
+  private boolean enabled = true;
+
+  private boolean ephemeral = true;
+
+  private String connectString = "zookeeper://127.0.0.1:2181";
+
+  private int connectionTimeoutMills = 1000;
+
+  private int sessionTimeoutMills = 60000;
+
+  private boolean enableSwaggerRegistration = false;
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public boolean isEphemeral() {
+    return ephemeral;
+  }
+
+  public void setEphemeral(boolean ephemeral) {
+    this.ephemeral = ephemeral;
+  }
+
+  public String getConnectString() {
+    return connectString;
+  }
+
+  public void setConnectString(String connectString) {
+    this.connectString = connectString;
+  }
+
+  public int getConnectionTimeoutMills() {
+    return connectionTimeoutMills;
+  }
+
+  public void setConnectionTimeoutMills(int connectionTimeoutMills) {
+    this.connectionTimeoutMills = connectionTimeoutMills;
+  }
+
+  public int getSessionTimeoutMills() {
+    return sessionTimeoutMills;
+  }
+
+  public void setSessionTimeoutMills(int sessionTimeoutMills) {
+    this.sessionTimeoutMills = sessionTimeoutMills;
+  }
+
+  public boolean isEnableSwaggerRegistration() {
+    return enableSwaggerRegistration;
+  }
+
+  public void setEnableSwaggerRegistration(boolean enableSwaggerRegistration) {
+    this.enableSwaggerRegistration = enableSwaggerRegistration;
+  }
+}
