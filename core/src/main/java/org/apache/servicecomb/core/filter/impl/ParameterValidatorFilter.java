de95c3c2e1634e2de6df93450b5dee1accd979b0
chengyouling <spadgerlin888@163.com>
2024-05-21 11:09:13 +0800

    remove import

--- core/src/main/java/org/apache/servicecomb/core/filter/impl/ParameterValidatorFilter.java
+++ core/src/main/java/org/apache/servicecomb/core/filter/impl/ParameterValidatorFilter.java
@@ -37,7 +37,6 @@ import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpo
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.core.env.Environment;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
