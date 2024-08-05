5ac4841633fc09a62327e9762e47bd9d45e614a0
liubao <bismy@qq.com>
2024-08-05 10:31:02 +0800

    [#3008]add tests and documents that header should encoded when not ASCII

--- demo/demo-springmvc/springmvc-client/src/main/java/org/apache/servicecomb/demo/springmvc/client/TestControllerImpl.java
+++ demo/demo-springmvc/springmvc-client/src/main/java/org/apache/servicecomb/demo/springmvc/client/TestControllerImpl.java
@@ -18,23 +18,53 @@ package org.apache.servicecomb.demo.springmvc.client;
 
 import org.apache.servicecomb.demo.CategorizedTestCase;
 import org.apache.servicecomb.demo.TestMgr;
+import org.apache.servicecomb.demo.controller.Controller;
 import org.apache.servicecomb.demo.controller.Person;
 import org.apache.servicecomb.demo.controller.PersonAlias;
 import org.apache.servicecomb.foundation.common.LegacyPropertyFactory;
+import org.apache.servicecomb.http.client.common.HttpUtils;
 import org.apache.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
-
 @Component
 public class TestControllerImpl implements CategorizedTestCase {
-  RestOperations restTemplate = RestTemplateBuilder.create();
-
   private static final String SERVER = "servicecomb://springmvc";
 
+  @Autowired
+  Controller controller;
+
+  RestOperations restTemplate = RestTemplateBuilder.create();
+
+  @Override
   public void testRestTransport() throws Exception {
     testQueryParamSpecial();
     testResponseModel();
+  }
+
+  @Override
+  public void testAllTransport() throws Exception {
+    testControllerBodyModel();
+    testControllerHeaderModel();
+  }
+
+  private void testControllerHeaderModel() throws Exception {
+    // HTTP header只允许 ASCII 字符。
+    // 在Header参数中使用特殊字符，需要在发送请求前进行编码，在收到响应后进行解码，具体编码方式可以业务自行选择。
+    // 如果不进行编码，则可能会出现乱码（HTTP协议历史上只标准化了ASCII字符，其他字符集可能工作，可能不工作）。
+    String encodedResult = controller.sayHei(HttpUtils.encodeURLParam("中文HTTP Header"));
+    TestMgr.check("hei 中文HTTP Header", HttpUtils.decodeURLParam(encodedResult));
+
+    // 普通ASCII字符不需要编码
+    TestMgr.check("hei ~!@#$%^&*()_+-={}[]|\\:\";''<>?,./AL340",
+        controller.sayHei("~!@#$%^&*()_+-={}[]|\\:\";''<>?,./AL340"));
+  }
+
+  private void testControllerBodyModel() {
+    Person user = new Person();
+    user.setName("world");
+    TestMgr.check("ha world", controller.saySomething("ha", user));
   }
 
   private void testResponseModel() {
