0abd777e8fab0b31931ee3b882d214f13283c636
yinhao <642283766@qq.com>
2024-10-16 21:49:05 +0800

    change return type of create method in DynamicPropertiesSource (#4560)

--- dynamic-config/config-zookeeper/src/main/java/org/apache/servicecomb/config/zookeeper/ZookeeperDynamicPropertiesSource.java
+++ dynamic-config/config-zookeeper/src/main/java/org/apache/servicecomb/config/zookeeper/ZookeeperDynamicPropertiesSource.java
@@ -26,6 +26,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
 
 public class ZookeeperDynamicPropertiesSource implements DynamicPropertiesSource {
   public static final String SOURCE_NAME = "zookeeper";
@@ -60,7 +61,7 @@ public class ZookeeperDynamicPropertiesSource implements DynamicPropertiesSource
   }
 
   @Override
-  public MapPropertySource create(Environment environment) {
+  public PropertySource<?> create(Environment environment) {
     init(environment);
     return new MapPropertySource(SOURCE_NAME, valueCache);
   }
