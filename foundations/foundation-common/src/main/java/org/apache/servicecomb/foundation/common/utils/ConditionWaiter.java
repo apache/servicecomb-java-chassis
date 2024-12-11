74d2511ba7e1b4e5c62a6c28e3ea4ec75afebb58
SweetWuXiaoMei <sweetwuxiaomei@qq.com>
2024-12-11 22:23:09 +0800

    refactor(foundation): Move common utility classes to foundation-common module
    - Move ConditionWaiter and MuteExceptionUtil classes from service-registry and dynamic-config modules to foundation-common module - Update import paths in related modules - Optimize EtcdConfigIT test class, add retry mechanism and log output

--- foundations/foundation-common/src/main/java/org/apache/servicecomb/foundation/common/utils/ConditionWaiter.java
+++ foundations/foundation-common/src/main/java/org/apache/servicecomb/foundation/common/utils/ConditionWaiter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.servicecomb.registry.etcd;
+package org.apache.servicecomb.foundation.common.utils;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
