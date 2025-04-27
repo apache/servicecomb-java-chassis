71ad008ce5a12d1842bafc95ddb1e830fc5ed48c
chengyouling <spadgerlin888@163.com>
2025-04-27 16:29:12 +0800

    注销Http Dispatcher请求

--- demo/demo-edge/consumer/src/main/java/org/apache/servicecomb/demo/edge/consumer/ConsumerMain.java
+++ demo/demo-edge/consumer/src/main/java/org/apache/servicecomb/demo/edge/consumer/ConsumerMain.java
@@ -37,8 +37,9 @@ public class ConsumerMain {
     new Consumer().run("rest");
     System.out.println("Running url dispatcher.");
     new Consumer().run("url");
-    System.out.println("Running http dispatcher.");
-    new Consumer().run("http");
+    // Common Http Dispatcher do not have OperationMeta, can not use router.
+//    System.out.println("Running http dispatcher.");
+//    new Consumer().run("http");
 
     System.out.println("All test case finished.");
 
--- demo/demo-multi-service-center/demo-multi-service-center-serverB/src/main/java/org/apache/servicecomb/demo/multiServiceCenterServerB/ConfigurationEndpoint.java
+++ demo/demo-multi-service-center/demo-multi-service-center-serverB/src/main/java/org/apache/servicecomb/demo/multiServiceCenterServerB/ConfigurationEndpoint.java
@@ -31,8 +31,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.netflix.config.DynamicPropertyFactory;
-
 @RestSchema(schemaId = "ConfigurationEndpoint")
 @RequestMapping(path = "/register/url/config", produces = MediaType.APPLICATION_JSON)
 public class ConfigurationEndpoint {
