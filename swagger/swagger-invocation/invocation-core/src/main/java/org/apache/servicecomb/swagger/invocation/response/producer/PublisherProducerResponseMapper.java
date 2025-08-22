c0643d9096d999d5251f4fdc7c718f6d078a78af
chengyouling <spadgerlin888@163.com>
2025-08-22 16:42:48 +0800

    [#4873] unified Flowable conversion to prevent internal typecasting exceptions.

--- swagger/swagger-invocation/invocation-core/src/main/java/org/apache/servicecomb/swagger/invocation/response/producer/PublisherProducerResponseMapper.java
+++ swagger/swagger-invocation/invocation-core/src/main/java/org/apache/servicecomb/swagger/invocation/response/producer/PublisherProducerResponseMapper.java
@@ -18,6 +18,7 @@ package org.apache.servicecomb.swagger.invocation.response.producer;
 
 import org.apache.servicecomb.swagger.invocation.Response;
 import org.apache.servicecomb.swagger.invocation.sse.SseEventResponseEntity;
+import org.reactivestreams.Publisher;
 
 import io.reactivex.rxjava3.core.Flowable;
 import jakarta.ws.rs.core.Response.StatusType;
@@ -31,12 +32,14 @@ public class PublisherProducerResponseMapper implements ProducerResponseMapper {
 
   @Override
   public Response mapResponse(StatusType status, Object result) {
+    // Unified Flowable conversion to prevent internal typecasting exceptions.
+    final Flowable<?> flowableResult = result instanceof Flowable ?
+            (Flowable<?>) result : Flowable.fromPublisher(((Publisher<?>) result));
     if (shouldConstructEntity) {
-      Flowable<SseEventResponseEntity<?>> responseEntity = ((Flowable<?>) result).map(obj ->
-          new SseEventResponseEntity<>()
-              .data(obj));
+      Flowable<SseEventResponseEntity<?>> responseEntity = flowableResult
+          .map(obj -> new SseEventResponseEntity<>().data(obj));
       return Response.create(status, responseEntity);
     }
-    return Response.create(status, result);
+    return Response.create(status, flowableResult);
   }
 }
