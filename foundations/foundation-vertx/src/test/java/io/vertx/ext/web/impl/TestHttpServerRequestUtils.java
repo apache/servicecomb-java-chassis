5a4c6d68add79ec0a49b5452924e277b8ac31179
chengyouling <spadgerlin888@163.com>
2026-06-23 08:54:58 +0800

    fixed test

--- foundations/foundation-vertx/src/test/java/io/vertx/ext/web/impl/TestHttpServerRequestUtils.java
+++ foundations/foundation-vertx/src/test/java/io/vertx/ext/web/impl/TestHttpServerRequestUtils.java
@@ -36,6 +36,7 @@ public class TestHttpServerRequestUtils {
     HttpServerRequestInternal request = Mockito.mock(HttpServerRequestInternal.class);
     HttpServerRequestWrapper wrapper = new HttpServerRequestWrapper(request, AllowForwardHeaders.NONE, null);
     Mockito.when(request.scheme()).thenReturn("http");
+    Mockito.when(request.uri()).thenReturn("http://localhost:8080");
     Mockito.when(context.request()).thenReturn(wrapper);
     Mockito.when(request.authority()).thenReturn(HostAndPort.create("localhost", 8080));
     RequestBody requestBody = Mockito.mock(RequestBody.class);
